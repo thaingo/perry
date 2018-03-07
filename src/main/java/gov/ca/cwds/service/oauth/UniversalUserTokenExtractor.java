31b19ec2a7df3cc367aec787fe4e28fefd04abac
Richard Bach <rich.bach@osi.ca.gov>
2018-03-06 21:25:14 -0800

    debug info for get user

--- src/main/java/gov/ca/cwds/service/oauth/UniversalUserTokenExtractor.java
+++ src/main/java/gov/ca/cwds/service/oauth/UniversalUserTokenExtractor.java
@@ -1,16 +1,17 @@
 package gov.ca.cwds.service.oauth;
 
-import gov.ca.cwds.PerryProperties;
-import gov.ca.cwds.UniversalUserToken;
+import java.util.Map;
+import java.util.UUID;
+import javax.script.ScriptException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-
-import javax.script.ScriptException;
-import java.util.Map;
-import java.util.UUID;
+import gov.ca.cwds.PerryProperties;
+import gov.ca.cwds.UniversalUserToken;
 
 /**
  * Created by dmitry.rudenko on 7/28/2017.
@@ -19,11 +20,15 @@ import java.util.UUID;
 @Primary
 @Profile("prod")
 public class UniversalUserTokenExtractor implements PrincipalExtractor {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(UniversalUserTokenExtractor.class);
+
   private PerryProperties configuration;
 
   @Override
   public UniversalUserToken extractPrincipal(Map<String, Object> map) {
-      try {
+    try {
+      LOGGER.debug("Map:" + map);
       UniversalUserToken userToken = configuration.getIdentityProvider().getIdpMapping().map(map);
       userToken.setToken(generateToken());
       return userToken;
