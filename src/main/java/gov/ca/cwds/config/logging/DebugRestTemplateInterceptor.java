9f343942e9a310f5fa5cdcb3dbb2a7e5c16ba7dc
DmitriyRudenko <Dmitry.Rudenko@osi.ca.gov>
2018-03-23 10:01:03 -0700

    oauth2 state ignore option

---
+++ src/main/java/gov/ca/cwds/config/logging/DebugRestTemplateInterceptor.java
@@ -0,0 +1,30 @@
+package gov.ca.cwds.config.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.IOException;
+
+public class DebugRestTemplateInterceptor implements ClientHttpRequestInterceptor {
+
+    private final static Logger log = LoggerFactory.getLogger(DebugRestTemplateInterceptor.class);
+
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+        traceRequest(request, body);
+        return execution.execute(request, body);
+    }
+
+    private void traceRequest(HttpRequest request, byte[] body) throws IOException {
+        log.debug("===========================request begin================================================");
+        log.debug("URI         : {}", request.getURI());
+        log.debug("Method      : {}", request.getMethod());
+        log.debug("Headers     : {}", request.getHeaders());
+        log.debug("Request body: {}", new String(body, "UTF-8"));
+        log.debug("==========================request end================================================");
+    }
+}
