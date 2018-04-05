2d5a26010d35f82a246cfe776d01702f6d3431d5
Richard Bach <rich.bach@osi.ca.gov>
2018-04-05 13:26:28 -0700

    updates for docker config based on pull request comment

--- docker-perry/entrypoint.sh
+++ docker-perry/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-if ([ -z "$DEV_MODE" ] || ! $DEV_MODE); then
-  echo "PROD MODE"
+
+if ( ${COGNITO_MODE:?} ) ; then
+  echo "COGNITO MODE"
   PERRY_CONFIG="--spring.config.location=config/perry-cognito.yml"
+  JAVA_OPTS="-Dspring.profiles.active=cognito,liquibase"
+elif ([ -z "$DEV_MODE" ] || ! $DEV_MODE); then
+  echo "PROD MODE"
+  PERRY_CONFIG="--spring.config.location=config/perry-prod.yml"
   JAVA_OPTS="-Dspring.profiles.active=prod,liquibase"
 else
   echo "DEV MODE"
