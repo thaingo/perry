c3c821a51d3eb89265a9f59b478d2107188368fa
DmitriyRudenko <Dmitry.Rudenko@osi.ca.gov>
2018-04-23 17:51:51 -0700

    validate fix

--- src/main/java/gov/ca/cwds/web/custom/dev/DevLogoutUrlProvider.java
+++ src/main/java/gov/ca/cwds/web/custom/dev/DevLogoutUrlProvider.java
@@ -1,7 +1,12 @@
 package gov.ca.cwds.web.custom.dev;
 
 import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
 import gov.ca.cwds.web.LogoutUrlProvider;
+import gov.ca.cwds.web.error.AuthenticationEventListener;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -9,8 +14,13 @@ import org.springframework.stereotype.Component;
 @Profile("dev")
 public class DevLogoutUrlProvider implements LogoutUrlProvider {
 
+  private final static Log logger = LogFactory.getLog(DevLogoutUrlProvider.class);
+  @Autowired(required = false)
+  private HttpServletRequest request;
+
   @Override
   public Optional<String> apply(String callback) {
+    logger.info("!!!REQUEST URL: " + request.getRequestURL());
     return Optional.ofNullable(callback);
   }
 
