d7c129ab304b1f09c79708f47595b587d26e5d8f
DmitriyRudenko <Dmitry.Rudenko@osi.ca.gov>
2018-04-25 10:21:55 -0700

    oauth2 request customization fix

--- src/main/java/gov/ca/cwds/service/oauth/custom/OAuth2RequestHttpEntityFactory.java
+++ src/main/java/gov/ca/cwds/service/oauth/custom/OAuth2RequestHttpEntityFactory.java
@@ -1,8 +1,10 @@
 package gov.ca.cwds.service.oauth.custom;
 
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
@@ -10,14 +12,16 @@ import org.springframework.stereotype.Service;
 @Service
 @Profile("prod")
 public class OAuth2RequestHttpEntityFactory {
-  private Map<String, Function<String, HttpEntity>> entityProviders = new HashMap<>();
+  private Map<String, OAuth2RequestCustomizer> requestCustomizers;
 
-  void addHttpEntityProvider(String url, Function<String, HttpEntity> httpEntityProvider) {
-    entityProviders.put(url, httpEntityProvider);
+  @Autowired
+  public OAuth2RequestHttpEntityFactory(List<OAuth2RequestCustomizer> requestCustomizers) {
+    this.requestCustomizers = requestCustomizers.stream()
+        .collect(Collectors.toMap(customizer -> customizer.url, customizer -> customizer));
   }
 
   public HttpEntity build(String url, String accessToken) {
-    Function<String, HttpEntity> entityProvider = entityProviders.get(url);
+    Function<String, HttpEntity> entityProvider = requestCustomizers.get(url);
     if (entityProvider != null) {
       return entityProvider.apply(accessToken);
     }
