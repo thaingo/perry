89346f5fecabeb29807c21eb5a4fd5ed457e3107
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-04-27 16:39:07 -0700

    COG-30: Query caching is configured

--- src/main/java/gov/ca/cwds/idm/persistence/RoleRepository.java
+++ src/main/java/gov/ca/cwds/idm/persistence/RoleRepository.java
@@ -1,11 +1,23 @@
 package gov.ca.cwds.idm.persistence;
 
-import gov.ca.cwds.data.auth.ReadOnlyRepository;
-import org.springframework.data.jpa.repository.Query;
 import gov.ca.cwds.idm.persistence.model.Role;
 import java.util.List;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
-public interface RoleRepository extends ReadOnlyRepository<Role, String> {
+public interface RoleRepository extends CrudRepository<Role, String> {
+
   @Query("SELECT r.name FROM Role r order by r.name asc")
+  @Cacheable("roleNames")
   List<String> findRoleNames();
+
+  @Override
+  @CacheEvict(value = "roleNames")
+  <S extends Role> S save(S entity);
+
+  @Override
+  @CacheEvict(value = "roleNames")
+  void delete(String name);
 }
