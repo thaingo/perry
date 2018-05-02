084cfc886bed6667aa1ff3719edee21db0a0a3a8
DmitriyRudenko <Dmitry.Rudenko@osi.ca.gov>
2018-05-01 18:55:45 -0700

    bug fix

--- src/main/java/gov/ca/cwds/config/api/sp/SpApiAuthenticationErrorHandler.java
+++ src/main/java/gov/ca/cwds/config/api/sp/SpApiAuthenticationErrorHandler.java
@@ -1,7 +1,9 @@
 package gov.ca.cwds.config.api.sp;
 
+import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -9,7 +11,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class SpApiAuthenticationErrorHandler implements AuthenticationEntryPoint {
   @Override
-  public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) {
-    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+  public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+    response.getOutputStream().print(HttpStatus.UNAUTHORIZED.getReasonPhrase());
+    response.setStatus(HttpStatus.UNAUTHORIZED.value());
   }
 }
