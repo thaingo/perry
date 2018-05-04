ea14fb8516f57c42a1f73ba189b4f06b740e0e99
sorych <dmytro.sorych@osi.ca.gov>
2018-05-04 14:15:01 -0700

    COG-66 Mocked service List of Users

--- src/main/java/gov/ca/cwds/idm/IdmService.java
+++ src/main/java/gov/ca/cwds/idm/IdmService.java
@@ -1,73 +1,49 @@
 package gov.ca.cwds.idm;
 
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.RegionUtils;
-import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClient;
-import com.amazonaws.services.cognitoidp.model.AttributeType;
-import com.amazonaws.services.cognitoidp.model.ListUsersInGroupRequest;
-import com.amazonaws.services.cognitoidp.model.ListUsersInGroupResult;
-import com.amazonaws.services.cognitoidp.model.ListUsersRequest;
-import com.amazonaws.services.cognitoidp.model.UserType;
 import gov.ca.cwds.idm.dto.User;
-import org.apache.commons.collections.CollectionUtils;
+import org.joda.time.DateTime;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class IdmService {
 
   public List<User> getUsers() {
-    AWSCognitoIdentityProviderClient identityUserPoolProviderClient;
-
-    // omitted stuff...
-    // initialize the Cognito Provider client.  This is used to talk to the user pool
-    identityUserPoolProviderClient = new AWSCognitoIdentityProviderClient(new BasicAWSCredentials("AKIAJC2X3HJISHKSBITA", "2w7Ik6KGQtOHy/BqQPF07FFF6tmECKdCS/Ur153g")); // creds are loaded via variables that are supplied to my program dynamically
-    identityUserPoolProviderClient.setRegion(RegionUtils.getRegion("us-east-2")); // var loaded
-
-
-    // ...some code omitted
-    ListUsersRequest listUsersRequest = new ListUsersRequest();
-    listUsersRequest.withUserPoolId("us-east-2_sQK657Nsc"); // id of the userpool, look this up in Cognito console
-    //listUsersRequest.withFilter("sub=xyz");  // i THINK this is how the Filter works... the documentation is terribad
-    //listUsersRequest.withFilter("username = \"testFromCode\"");
-
-
-    // get the results
-    //ListUsersResult result = identityUserPoolProviderClient.listUsers(listUsersRequest);
-
-    ListUsersInGroupRequest listUsersInGroupRequest = new ListUsersInGroupRequest();
-    listUsersInGroupRequest.withGroupName("SuperCounty").withUserPoolId("us-east-2_sQK657Nsc");
-    ListUsersInGroupResult result = identityUserPoolProviderClient.listUsersInGroup(listUsersInGroupRequest);
-
-    List<UserType> userTypeList = result.getUsers();
-    List<User> resultList = new ArrayList<>(userTypeList.size());
-    // loop through them
-    for (UserType userType : userTypeList) {
-      resultList.add(convert(userType, "SuperCounty"));
-
-
+    List<User> resultList = new ArrayList<>();
+    for (int i = 0; i < 20; i++) {
+      resultList.add(createUser(i));
     }
     return resultList;
-  }
-
-
-  private User convert(UserType userType, String countyName) {
-    return new User(userType.getUsername(), countyName, convertAttributest(userType.getAttributes()));
-  }
-
-  private List<String> convertAttributest(List<AttributeType> attributes) {
-    return attributes.stream().filter(attributeType -> !attributeType.getName().equals("sub")).map(this::getAsString).collect(Collectors.toList());
-  }
-
-  private String getAsString(AttributeType attributeType) {
-    return attributeType.getName()+ ": " + attributeType.getValue() + " ";
   }
 
   public List getRoles() {
     return Collections.emptyList();
   }
+
+  private User createUser(int i) {
+    User user = new User();
+
+    user.setCountyName("MyCounty");
+    user.setUsername("userName" + i);
+    user.setEnabled(i % 2 == 0);
+    user.setEmail("email"+i+"@test.com");
+    user.setOffice("Office address "+i);
+    user.setPhoneNumber("+1916999999"+i%10);
+    user.setEndDate(new Date());
+    user.setStartDate(new Date());
+    user.setFirstName("Firstname" + i);
+    user.setLastName("Lastname" + i);
+    user.setRacfid("RACFID" + i);
+    user.setUserCreateDate(new Date());
+    user.setUserLastModifiedDate(new Date());
+    user.setStatus("userStatus" + i);
+    user.setLastLoginDateTime(LocalDateTime.now().minusHours(i).plusMinutes(i).minusDays(i+5));
+    return user;
+  }
+
 }
