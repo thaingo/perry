80494ccd69e0908745c3c12f529675127600f96e
DmitriyRudenko <Dmitry.Rudenko@osi.ca.gov>
2018-05-07 14:23:30 -0700

    multithreading

--- integration-tests/src/test/java/gov/ca/cwds/test/TestCognito.java
+++ integration-tests/src/test/java/gov/ca/cwds/test/TestCognito.java
@@ -1,24 +1,45 @@
 package gov.ca.cwds.test;
 
 import java.nio.charset.Charset;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import net.serenitybdd.junit.runners.SerenityParameterizedRunner;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
+import net.thucydides.junit.annotations.Concurrent;
+import net.thucydides.junit.annotations.TestData;
 import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
-@RunWith(SerenityRunner.class)
+@RunWith(SerenityParameterizedRunner.class)
+@Concurrent
 public class TestCognito {
 
-  @Managed(driver = "chrome")
+  @Managed(driver = "chrome", uniqueSession = true)
   private WebDriver driver;
   private String basePerryUrl;
 
   @Steps
   private LoginSteps loginSteps;
+
+  @TestData
+  public static Collection<Object[]> testData(){
+    int threadsCount = Integer.valueOf(System.getProperty("perry.threads.count", "4"));
+    return IntStream.range(0, threadsCount).boxed().map(i -> {
+      Object [] item = new Integer[1];
+      item[0] = i;
+      return item;
+    }).collect(Collectors.toList());
+  }
+
+  public TestCognito(int number) {
+
+  }
 
   @Before
   public void init() {
