1ce0baa2e96ab6b462bcc0619fbccb14cc9196fc
DmitriyRudenko <Dmitry.Rudenko@osi.ca.gov>
2018-05-07 14:20:49 -0700

    multithreading

--- integration-tests/src/test/java/gov/ca/cwds/test/TestDevMode.java
+++ integration-tests/src/test/java/gov/ca/cwds/test/TestDevMode.java
@@ -1,19 +1,27 @@
 package gov.ca.cwds.test;
 
 import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import net.serenitybdd.junit.runners.SerenityParameterizedRunner;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
+import net.thucydides.junit.annotations.Concurrent;
+import net.thucydides.junit.annotations.TestData;
 import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
-@RunWith(SerenityRunner.class)
+@RunWith(SerenityParameterizedRunner.class)
+@Concurrent
 public class TestDevMode {
 
-  @Managed(driver = "chrome")
+  @Managed(driver = "chrome", uniqueSession = true)
   private WebDriver driver;
   private String basePerryUrl;
 
@@ -24,6 +32,20 @@ public class TestDevMode {
   public void init() {
     loginSteps.setDriver(driver);
     basePerryUrl = System.getProperty("perry.url", "http://localhost:8080/perry");
+  }
+
+  @TestData
+  public static Collection<Object[]> testData(){
+    int threadsCount = Integer.valueOf(System.getProperty("perry.threads.count", "4"));
+    return IntStream.range(0, threadsCount).boxed().map(i -> {
+      Object [] item = new Integer[1];
+      item[0] = i;
+      return item;
+    }).collect(Collectors.toList());
+  }
+
+  public TestDevMode(int number) {
+
   }
 
   @Test
