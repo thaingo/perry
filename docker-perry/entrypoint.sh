a2a13055e75a3d6d34b2a3ca19f1917d3eff2241
Leonid Marushevskyi <leonid.marushevskiy@osi.ca.gov>
2018-05-09 12:53:00 -0700

    COG-55: Swagger profile enabled only for DEV Mode

--- docker-perry/entrypoint.sh
+++ docker-perry/entrypoint.sh
@@ -4,11 +4,11 @@
 if ( ${COGNITO_MODE:?} ) ; then
   echo "COGNITO MODE"
   PERRY_CONFIG="--spring.config.location=config/perry-prod.yml"
-  JAVA_OPTS="-Dspring.profiles.active=prod,cognito,liquibase,swagger"
+  JAVA_OPTS="-Dspring.profiles.active=prod,cognito,liquibase"
 elif ([ -z "$DEV_MODE" ] || ! $DEV_MODE); then
   echo "PROD MODE"
   PERRY_CONFIG="--spring.config.location=config/perry-prod.yml"
-  JAVA_OPTS="-Dspring.profiles.active=prod,saf,liquibase,swagger"
+  JAVA_OPTS="-Dspring.profiles.active=prod,saf,liquibase"
 else
   echo "DEV MODE"
   PERRY_CONFIG="--spring.config.location=config/perry-dev.yml"
