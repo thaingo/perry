4e8ab7bc822d3da4f446fb422c600b5331bb3c9a
Volodymyr Petrusha <volodymyr.petrusha@osi.ca.gov>
2018-05-10 12:34:23 -0700

    COG-30 CAP-API Retrieve a list of permissions
    PermissionRepository refactored

--- src/main/java/gov/ca/cwds/idm/persistence/PermissionRepository.java
+++ src/main/java/gov/ca/cwds/idm/persistence/PermissionRepository.java
@@ -1,23 +1,13 @@
 package gov.ca.cwds.idm.persistence;
 
+import gov.ca.cwds.data.auth.ReadOnlyRepository;
 import gov.ca.cwds.idm.persistence.model.Permission;
 import java.util.List;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
-public interface PermissionRepository extends CrudRepository<Permission, String> {
+public interface PermissionRepository extends ReadOnlyRepository<Permission, String> {
 
   @Query("SELECT p.name FROM Permission p order by p.name asc")
-  @Cacheable("permissionNames")
   List<String> findPermissionNames();
 
-  @Override
-  @CacheEvict(value = "permissionNames")
-  <S extends Permission> S save(S entity);
-
-  @Override
-  @CacheEvict(value = "permissionNames")
-  void delete(String name);
 }
