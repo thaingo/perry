c54861d1f6a6ac1804c6cf0074275ce6eee2d7e7
Leonid Marushevskyi <leonid.marushevskiy@osi.ca.gov>
2018-05-14 10:42:28 -0700

    COG-55: Sonar fixes

--- src/main/java/gov/ca/cwds/config/SwaggerConfig.java
+++ src/main/java/gov/ca/cwds/config/SwaggerConfig.java
@@ -19,12 +19,6 @@ import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.DocExpansion;
-import springfox.documentation.swagger.web.ModelRendering;
-import springfox.documentation.swagger.web.OperationsSorter;
-import springfox.documentation.swagger.web.TagsSorter;
-import springfox.documentation.swagger.web.UiConfiguration;
-import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -73,23 +67,4 @@ public class SwaggerConfig {
     return newArrayList(new SecurityReference(AUTHORIZATION, authorizationScopes));
   }
 
-  @Bean
-  UiConfiguration uiConfig() {
-    return UiConfigurationBuilder.builder()
-        .deepLinking(true)
-        .displayOperationId(false)
-        .defaultModelsExpandDepth(1)
-        .defaultModelExpandDepth(1)
-        .defaultModelRendering(ModelRendering.EXAMPLE)
-        .displayRequestDuration(true)
-        .docExpansion(DocExpansion.NONE)
-        .filter(false)
-        .maxDisplayedTags(null)
-        .operationsSorter(OperationsSorter.ALPHA)
-        .showExtensions(false)
-        .tagsSorter(TagsSorter.ALPHA)
-        .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
-        .validatorUrl(null)
-        .build();
-  }
 }
