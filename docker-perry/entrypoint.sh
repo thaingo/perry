b78eb421abbb83dbb8179bd27125e522a22bf7f6
Leonid Marushevskyi <leonid.marushevskiy@osi.ca.gov>
2018-05-14 07:53:05 -0700

    COG-55: New versions of dependencies. Endpoints marked as secured in Swagger

--- docker-perry/entrypoint.sh
+++ docker-perry/entrypoint.sh
@@ -4,11 +4,19 @@
 if ( ${COGNITO_MODE:?} ) ; then
   echo "COGNITO MODE"
   PERRY_CONFIG="--spring.config.location=config/perry-prod.yml"
-  JAVA_OPTS="-Dspring.profiles.active=prod,cognito,liquibase"
+  if [ "$SWAGGER" = true ] ; then
+    JAVA_OPTS="-Dspring.profiles.active=prod,cognito,liquibase,swagger"
+  else
+    JAVA_OPTS="-Dspring.profiles.active=prod,cognito,liquibase"
+  fi
 elif ([ -z "$DEV_MODE" ] || ! $DEV_MODE); then
   echo "PROD MODE"
   PERRY_CONFIG="--spring.config.location=config/perry-prod.yml"
-  JAVA_OPTS="-Dspring.profiles.active=prod,saf,liquibase"
+  if [ "$SWAGGER" = true ] ; then
+    JAVA_OPTS="-Dspring.profiles.active=prod,saf,liquibase,swagger"
+  else
+    JAVA_OPTS="-Dspring.profiles.active=prod,saf,liquibase"
+  fi
 else
   echo "DEV MODE"
   PERRY_CONFIG="--spring.config.location=config/perry-dev.yml"
--- src/main/java/gov/ca/cwds/config/SwaggerConfig.java
+++ src/main/java/gov/ca/cwds/config/SwaggerConfig.java
@@ -2,15 +2,29 @@ package gov.ca.cwds.config;
 
 import static com.google.common.base.Predicates.not;
 import static com.google.common.base.Predicates.or;
+import static com.google.common.collect.Lists.newArrayList;
 import static springfox.documentation.builders.PathSelectors.regex;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.DocExpansion;
+import springfox.documentation.swagger.web.ModelRendering;
+import springfox.documentation.swagger.web.OperationsSorter;
+import springfox.documentation.swagger.web.TagsSorter;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -18,23 +32,64 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Profile("swagger")
 public class SwaggerConfig {
 
-  private static final Contact DEFAULT_CONTACT = new Contact(
-      "", "", "");
+  private static final Contact DEFAULT_CONTACT = new Contact("", "", "");
 
-  private static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
-      "Perry API", "RESTful Perry API", "",
-      "", DEFAULT_CONTACT,
-      "", "");
+  private static final ApiInfo DEFAULT_API_INFO =
+      new ApiInfo(
+          "Perry API", "RESTful Perry API", "", "", DEFAULT_CONTACT, "", "", new ArrayList<>());
+  public static final String AUTHORIZATION = "Authorization";
 
   @Bean
   public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
         .apiInfo(DEFAULT_API_INFO)
         .select()
-        .paths(not(or(
-            regex("/error"),// Exclude Spring error controller
-            regex("/") //Exclude Home Page Url controller
-        )))
+        .paths(
+            not(
+                or(
+                    regex("/error"), // Exclude Spring error controller
+                    regex("/") // Exclude Home Page Url controller
+                    )))
+        .build()
+        .securitySchemes(newArrayList(apiKey()))
+        .securityContexts(newArrayList(securityContext()));
+  }
+
+  private ApiKey apiKey() {
+    return new ApiKey(AUTHORIZATION, AUTHORIZATION, "header");
+  }
+
+  private SecurityContext securityContext() {
+    return SecurityContext.builder()
+        .securityReferences(defaultAuth())
+        .forPaths(PathSelectors.any())
+        .build();
+  }
+
+  List<SecurityReference> defaultAuth() {
+    AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+    authorizationScopes[0] = authorizationScope;
+    return newArrayList(new SecurityReference(AUTHORIZATION, authorizationScopes));
+  }
+
+  @Bean
+  UiConfiguration uiConfig() {
+    return UiConfigurationBuilder.builder()
+        .deepLinking(true)
+        .displayOperationId(false)
+        .defaultModelsExpandDepth(1)
+        .defaultModelExpandDepth(1)
+        .defaultModelRendering(ModelRendering.EXAMPLE)
+        .displayRequestDuration(true)
+        .docExpansion(DocExpansion.NONE)
+        .filter(false)
+        .maxDisplayedTags(null)
+        .operationsSorter(OperationsSorter.ALPHA)
+        .showExtensions(false)
+        .tagsSorter(TagsSorter.ALPHA)
+        .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+        .validatorUrl(null)
         .build();
   }
 }
