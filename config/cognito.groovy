dfa2c6b6d9e12cdd78502bc8006922960b7e113c
DmitriyRudenko <Dmitry.Rudenko@osi.ca.gov>
2018-05-16 16:57:45 -0700

    mapping fix

--- config/cognito.groovy
+++ config/cognito.groovy
@@ -2,6 +2,10 @@ def attribute = {name -> idpToken.UserAttributes?.find {it.Name.equalsIgnoreCase
 
 universalUserToken.userId = attribute("CUSTOM:RACFID")?.toUpperCase()
 
+if(!universalUserToken.userId?.trim()) {
+    universalUserToken.userId = attribute("email")
+}
+
 universalUserToken.roles = attribute("zoneinfo")?.split('\\s*:\\s*') as HashSet
 
 if(!universalUserToken.roles) {
