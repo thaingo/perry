177f119421aa83c2f5a9eb246ea954600d01a0dc
DmitriyRudenko <Dmitry.Rudenko@osi.ca.gov>
2018-05-21 14:01:47 -0700

    access code bug fix

--- src/test/java/gov/ca/cwds/rest/api/LoginResourceTest.java
+++ src/test/java/gov/ca/cwds/rest/api/LoginResourceTest.java
@@ -4,10 +4,18 @@ import org.junit.Test;
 
 public class LoginResourceTest {
   @Test
-  public void testAccessCode() {
+  public void testAccessCodeWithParameters() {
     LoginResource loginResource = new LoginResource();
     String callback = "http://host:8080/context/path?param1=value1";
     String urlWithAccessCode = loginResource.addAccessCode(callback, "123");
     assert urlWithAccessCode.equals("http://host:8080/context/path?param1=value1&accessCode=123");
+  }
+
+  @Test
+  public void testAccessCodeWithoutParameters() {
+    LoginResource loginResource = new LoginResource();
+    String callback = "http://host:8080/context/path";
+    String urlWithAccessCode = loginResource.addAccessCode(callback, "123");
+    assert urlWithAccessCode.equals("http://host:8080/context/path?accessCode=123");
   }
 }
