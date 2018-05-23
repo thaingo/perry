5ce4087bd84337e9a49af659513df88ab44d79bc
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-05-23 15:47:18 -0700

    COG-92: CognitoUtils is created

---
+++ src/main/java/gov/ca/cwds/idm/service/CognitoUtils.java
@@ -0,0 +1,57 @@
+package gov.ca.cwds.idm.service;
+
+import com.amazonaws.services.cognitoidp.model.AttributeType;
+import com.amazonaws.services.cognitoidp.model.UserType;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import liquibase.util.StringUtils;
+import org.apache.commons.collections.CollectionUtils;
+
+public class CognitoUtils {
+
+  static final String PERMISSIONS_ATTR_NAME = "custom:permission";
+  static final String PERMISSIONS_DELIMITER = ":";
+
+  private CognitoUtils() {
+  }
+
+  public static Optional<AttributeType> getAttribute(UserType cognitoUser, String attrName) {
+    List<AttributeType> attributes = cognitoUser.getAttributes();
+
+    if(CollectionUtils.isEmpty(attributes)) {
+      return Optional.empty();
+    } else {
+      return attributes.stream().filter(attr -> attr.getName().equals(attrName))
+          .findFirst();
+    }
+  }
+
+  public static Set<String> getPermissions(UserType cognitoUser) {
+
+    Optional<AttributeType> permissionsAttrOpt = getAttribute(cognitoUser, PERMISSIONS_ATTR_NAME);
+
+    if(! permissionsAttrOpt.isPresent()) {
+      return new HashSet<>();
+    }
+
+    AttributeType permissionsAttr = permissionsAttrOpt.get();
+    String permissionsStr = permissionsAttr.getValue();
+
+    if(StringUtils.isEmpty(permissionsStr)){
+      return new HashSet<>();
+    }
+
+    return new HashSet<>(Arrays.asList(permissionsStr.split(PERMISSIONS_DELIMITER)));
+  }
+
+  public static String getPermissionsAttributeValue(Set<String> permissions) {
+    if(CollectionUtils.isNotEmpty(permissions)) {
+      return String.join(PERMISSIONS_DELIMITER, permissions);
+    } else {
+      return "";
+    }
+  }
+}
---
+++ src/test/java/gov/ca/cwds/idm/service/CognitoUtilsTest.java
@@ -0,0 +1,149 @@
+package gov.ca.cwds.idm.service;
+
+import static gov.ca.cwds.idm.service.CognitoUtils.PERMISSIONS_ATTR_NAME;
+import static gov.ca.cwds.idm.service.CognitoUtils.getAttribute;
+import static gov.ca.cwds.idm.service.CognitoUtils.getPermissions;
+import static gov.ca.cwds.idm.service.CognitoUtils.getPermissionsAttributeValue;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import com.amazonaws.services.cognitoidp.model.AttributeType;
+import com.amazonaws.services.cognitoidp.model.UserType;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import org.junit.Test;
+
+public class CognitoUtilsTest {
+
+  @Test
+  public void testGetAttributeNoAttributes() {
+    UserType cognitoUser = new UserType();
+    Optional<AttributeType> attrOpt = getAttribute(cognitoUser, "someName");
+    assertThat(attrOpt.isPresent(), is(false));
+  }
+
+  @Test
+  public void testGetAttributeEmptyAttributes() {
+    UserType cognitoUser = new UserType();
+    cognitoUser.withAttributes();
+    Optional<AttributeType> attrOpt = getAttribute(cognitoUser, "someName");
+    assertThat(attrOpt.isPresent(), is(false));
+  }
+
+  @Test
+  public void testGetAttributeOtherAttribute() {
+    UserType cognitoUser = new UserType();
+
+    AttributeType otherAttr = new AttributeType();
+    otherAttr.setName("otherName");
+    otherAttr.setValue("otherValue");
+
+    cognitoUser.withAttributes(otherAttr);
+
+    Optional<AttributeType> attrOpt = getAttribute(cognitoUser, "someName");
+    assertThat(attrOpt.isPresent(), is(false));
+  }
+
+  @Test
+  public void testGetAttributeValidAttribute() {
+    UserType cognitoUser = new UserType();
+
+    AttributeType validAttr = new AttributeType();
+    validAttr.setName("someName");
+    validAttr.setValue("someValue");
+
+    AttributeType otherAttr = new AttributeType();
+    otherAttr.setName("otherName");
+    otherAttr.setValue("otherValue");
+
+    cognitoUser.withAttributes(validAttr, otherAttr);
+
+    Optional<AttributeType> attrOpt = getAttribute(cognitoUser, "someName");
+    assertTrue(attrOpt.isPresent());
+    AttributeType attr = attrOpt.get();
+    assertThat(attr.getName(), is("someName"));
+    assertThat(attr.getValue(), is("someValue"));
+  }
+
+  @Test
+  public void testGetNoPermissions() {
+
+    UserType cognitoUser = new UserType();
+
+    AttributeType otherAttr = new AttributeType();
+    otherAttr.setName("otherName");
+    otherAttr.setValue("otherValue");
+
+    cognitoUser.withAttributes(otherAttr);
+
+    assertThat(getPermissions(cognitoUser), empty());
+  }
+
+  @Test
+  public void testGetNullPermissions() {
+
+    UserType cognitoUser = new UserType();
+
+    AttributeType permissionsAttr = new AttributeType();
+    permissionsAttr.setName(PERMISSIONS_ATTR_NAME);
+    permissionsAttr.setValue(null);
+
+    cognitoUser.withAttributes(permissionsAttr);
+
+    assertThat(getPermissions(cognitoUser), empty());
+  }
+
+  @Test
+  public void testGetEmptyPermissions() {
+
+    UserType cognitoUser = new UserType();
+
+    AttributeType permissionsAttr = new AttributeType();
+    permissionsAttr.setName(PERMISSIONS_ATTR_NAME);
+    permissionsAttr.setValue("");
+
+    cognitoUser.withAttributes(permissionsAttr);
+
+    assertThat(getPermissions(cognitoUser), empty());
+  }
+
+  @Test
+  public void testGetPermissions() {
+    UserType cognitoUser = new UserType();
+
+    AttributeType permissionsAttr = new AttributeType();
+    permissionsAttr.setName(PERMISSIONS_ATTR_NAME);
+    permissionsAttr.setValue("Snapshot-rollout:Hotline-rollout");
+
+    cognitoUser.withAttributes(permissionsAttr);
+
+    Set<String> permissions = getPermissions(cognitoUser);
+    assertThat(permissions, hasSize(2));
+    assertThat(permissions, hasItem("Snapshot-rollout"));
+    assertThat(permissions, hasItem("Hotline-rollout"));
+  }
+
+  @Test
+  public void testGetPermissionsAttributeValueNull() {
+    assertThat(getPermissionsAttributeValue(null), is(""));
+  }
+
+  @Test
+  public void testGetPermissionsAttributeValueEmpty() {
+    Set<String> permissions = new HashSet<>();
+    assertThat(getPermissionsAttributeValue(permissions), is(""));
+  }
+
+  @Test
+  public void testGetPermissionsAttributeValue() {
+    Set<String> permissions = new HashSet<>();
+    permissions.add("one");
+    permissions.add("two");
+    assertThat(getPermissionsAttributeValue(permissions), is("one:two"));
+  }
+}
