8efa8f51c07fd0bb9d43fbf61cc23de2e10b1e46
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-05-31 16:53:42 -0700

    in-memorty database creation is moved into setApplicationContext() method

--- src/test/java/gov/ca/cwds/idm/BaseTokenStoreLiquibaseTest.java
+++ src/test/java/gov/ca/cwds/idm/BaseTokenStoreLiquibaseTest.java
@@ -10,13 +10,13 @@ import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.DatabaseException;
 import liquibase.exception.LiquibaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
-import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.BeansException;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationContextException;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -28,19 +28,18 @@ public abstract class BaseTokenStoreLiquibaseTest implements ApplicationContextA
   private static final String SPRING_BOOT_H2_PASSWORD = "";
   private static final String CHANGE_LOG = "liquibase/perry_database_master.xml";
 
-  //usually "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE";
-  private String springBootH2url;
-
-  @Before
-  public void beforeSuper() throws Exception {
-    createDatabase(springBootH2url);
-  }
-
   @Override
   public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
     DataSourceProperties datasourceProperties =
         applicationContext.getBean("tokenStoreDatasourceProperties", DataSourceProperties.class);
-    springBootH2url = datasourceProperties.determineUrl();
+
+    //usually "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE";
+    String springBootH2url = datasourceProperties.determineUrl();
+    try {
+      createDatabase(springBootH2url);
+    } catch (Exception e) {
+      throw new ApplicationContextException("error at creating in-memory database", e);
+    }
   }
 
   private static void createDatabase(String url) throws Exception {
