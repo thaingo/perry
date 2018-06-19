a4c0908981820536ba656fa41441708a4cac93b8
sorych <dmytro.sorych@osi.ca.gov>
2018-06-19 16:37:20 -0700

    unit tests added

--- src/test/java/gov/ca/cwds/service/UserAuthorizationServiceTest.java
+++ src/test/java/gov/ca/cwds/service/UserAuthorizationServiceTest.java
@@ -1,38 +1,52 @@
 package gov.ca.cwds.service;
 
-import gov.ca.cwds.data.auth.*;
-import gov.ca.cwds.data.persistence.auth.*;
+import gov.ca.cwds.data.auth.AssignmentUnitDao;
+import gov.ca.cwds.data.auth.CwsOfficeDao;
+import gov.ca.cwds.data.auth.StaffAuthorityPrivilegeDao;
+import gov.ca.cwds.data.auth.StaffPersonDao;
+import gov.ca.cwds.data.auth.StaffUnitAuthorityDao;
+import gov.ca.cwds.data.auth.UserIdDao;
+import gov.ca.cwds.data.persistence.auth.AssignmentUnit;
 import gov.ca.cwds.data.persistence.auth.CwsOffice;
 import gov.ca.cwds.data.persistence.auth.StaffAuthorityPrivilege;
+import gov.ca.cwds.data.persistence.auth.StaffPerson;
 import gov.ca.cwds.data.persistence.auth.StaffUnitAuthority;
-import gov.ca.cwds.rest.api.domain.auth.*;
+import gov.ca.cwds.data.persistence.auth.UserId;
+import gov.ca.cwds.rest.api.domain.auth.UserAuthorization;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.util.Collections;
 import java.util.Date;
 
-/**
- * Created by dmitry.rudenko on 10/3/2017.
- */
+/** Created by dmitry.rudenko on 10/3/2017. */
 public class UserAuthorizationServiceTest {
 
-  public static final String STAFF_ID = "staffId";
-  public static final String ID = "id";
-  public static final String LOGON_ID = "logonId";
-  public static final String UNIT = "unit";
-  public static final String CWSOFFICE_ID = "cwsofficeId";
-  public static final String COUNTY_CODE = "12";
-  public static final short COUNTY_CWS_CODE = 1079;
+  private static final String STAFF_ID = "staffId";
+  private static final String ID = "id";
+  private static final String LOGON_ID = "logonId";
+  private static final String UNIT = "unit";
+  private static final String CWSOFFICE_ID = "cwsofficeId";
+  private static final String COUNTY_CODE = "12";
+  private static final short COUNTY_CWS_CODE = 1079;
 
-  @Test
-  public void test() {
-    UserIdDao userIdDao = Mockito.mock(UserIdDao.class);
-    StaffAuthorityPrivilegeDao staffAuthorityPrivilegeDao = Mockito.mock(StaffAuthorityPrivilegeDao.class);
-    StaffUnitAuthorityDao staffUnitAuthorityDao = Mockito.mock(StaffUnitAuthorityDao.class);
-    CwsOfficeDao cwsOfficeDao = Mockito.mock(CwsOfficeDao.class);
-    AssignmentUnitDao assignmentUnitDao = Mockito.mock(AssignmentUnitDao.class);
-    StaffPersonDao staffPersonDao = Mockito.mock(StaffPersonDao.class);
+  private UserIdDao userIdDao;
+  private StaffAuthorityPrivilegeDao staffAuthorityPrivilegeDao;
+  private StaffUnitAuthorityDao staffUnitAuthorityDao;
+  private CwsOfficeDao cwsOfficeDao;
+  private AssignmentUnitDao assignmentUnitDao;
+  private StaffPersonDao staffPersonDao;
+  private UserAuthorizationService userAuthorizationService;
+
+  @Before
+  public void init() {
+    userIdDao = Mockito.mock(UserIdDao.class);
+    staffAuthorityPrivilegeDao = Mockito.mock(StaffAuthorityPrivilegeDao.class);
+    staffUnitAuthorityDao = Mockito.mock(StaffUnitAuthorityDao.class);
+    cwsOfficeDao = Mockito.mock(CwsOfficeDao.class);
+    assignmentUnitDao = Mockito.mock(AssignmentUnitDao.class);
+    staffPersonDao = Mockito.mock(StaffPersonDao.class);
 
     CwsUserInfoService cwsUserInfoService = new CwsUserInfoService();
     cwsUserInfoService.setAssignmentUnitDao(assignmentUnitDao);
@@ -42,16 +56,20 @@ public class UserAuthorizationServiceTest {
     cwsUserInfoService.setStaffUnitAuthorityDao(staffUnitAuthorityDao);
     cwsUserInfoService.setStaffPersonDao(staffPersonDao);
 
-    UserAuthorizationService userAuthorizationService = new UserAuthorizationService();
+    userAuthorizationService = new UserAuthorizationService();
     userAuthorizationService.setCwsUserInfoService(cwsUserInfoService);
+  }
+
+  @Test
+  public void findByRacfIdTest() {
 
     String racfid = "racfid";
     UserId userId = new UserId(null, null, null, STAFF_ID, ID, LOGON_ID, null);
     Mockito.when(userIdDao.findActiveByLogonId(racfid))
-            .thenReturn(Collections.singletonList(userId));
+        .thenReturn(Collections.singletonList(userId));
 
     Mockito.when(staffAuthorityPrivilegeDao.findSocialWorkerPrivileges(userId.getId()))
-            .thenReturn(Collections.emptyList());
+        .thenReturn(Collections.emptyList());
 
     StaffPerson staffPerson = new StaffPerson();
     staffPerson.setId(STAFF_ID);
@@ -59,27 +77,48 @@ public class UserAuthorizationServiceTest {
 
     Mockito.when(staffPersonDao.findOne(STAFF_ID)).thenReturn(staffPerson);
 
-    StaffAuthorityPrivilege staffAuthorityPrivilege = new StaffAuthorityPrivilege(
+    StaffAuthorityPrivilege staffAuthorityPrivilege =
+        new StaffAuthorityPrivilege(
             COUNTY_CODE, null, null, null, null, "L", (short) 1, null, null);
 
     Mockito.when(staffAuthorityPrivilegeDao.findByUserId(userId.getId()))
-            .thenReturn(Collections.singletonList(staffAuthorityPrivilege));
+        .thenReturn(Collections.singletonList(staffAuthorityPrivilege));
 
-    StaffUnitAuthority staffUnitAuthority = new StaffUnitAuthority(
-            "S", null, new Date(), UNIT, STAFF_ID, new Date(), null);
+    StaffUnitAuthority staffUnitAuthority =
+        new StaffUnitAuthority("S", null, new Date(), UNIT, STAFF_ID, new Date(), null);
 
     Mockito.when(staffUnitAuthorityDao.findByStaffPersonId(STAFF_ID))
-            .thenReturn(Collections.singletonList(staffUnitAuthority));
+        .thenReturn(Collections.singletonList(staffUnitAuthority));
 
-    AssignmentUnit assignmentUnit = new AssignmentUnit(
+    AssignmentUnit assignmentUnit =
+        new AssignmentUnit(
             null, 0, null, COUNTY_CODE, new Date(), CWSOFFICE_ID, null, new Date(), null);
 
-    Mockito.when(assignmentUnitDao.findOne(UNIT))
-            .thenReturn(assignmentUnit);
+    Mockito.when(assignmentUnitDao.findOne(UNIT)).thenReturn(assignmentUnit);
 
-    CwsOffice cwsOffice = new CwsOffice(
-            CWSOFFICE_ID, null, null, COUNTY_CWS_CODE , null, null, null, null, null, null, null, null, null, null, null, null, null,
-            COUNTY_CODE, null, null, null);
+    CwsOffice cwsOffice =
+        new CwsOffice(
+            CWSOFFICE_ID,
+            null,
+            null,
+            COUNTY_CWS_CODE,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            COUNTY_CODE,
+            null,
+            null,
+            null);
 
     Mockito.when(cwsOfficeDao.findOne(CWSOFFICE_ID)).thenReturn(cwsOffice);
 
@@ -89,12 +128,12 @@ public class UserAuthorizationServiceTest {
     assert userAuthorization.getStaffPerson().equals(staffPerson);
     assert !userAuthorization.getSocialWorker();
     assert userAuthorization.getAuthorityPrivilege().size() == 1;
-    gov.ca.cwds.rest.api.domain.auth.StaffAuthorityPrivilege authorityPrivilege = userAuthorization.getAuthorityPrivilege().iterator().next();
+    gov.ca.cwds.rest.api.domain.auth.StaffAuthorityPrivilege authorityPrivilege =
+        userAuthorization.getAuthorityPrivilege().iterator().next();
     assert authorityPrivilege.getAuthPrivilegeCode().equals("L");
     assert authorityPrivilege.getAuthPrivilegeType().equals("1");
     assert authorityPrivilege.getCounty().equals("Humboldt");
     assert authorityPrivilege.getCountyCode().equals(COUNTY_CODE);
     assert authorityPrivilege.getAuthPrivilegeCodeDesc().equals("Staff Person Level of Auth Type");
-
   }
 }
