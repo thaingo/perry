c708f1daba49e40b7f014bad10600a37bf6c6b87
sorych <dmytro.sorych@osi.ca.gov>
2018-07-13 15:10:12 -0700

    Revert "COG-268 getUsers refactored to return all users by pages"
    This reverts commit 8be31d16f589372d2afaac48615441ad6879e3ea.

--- src/main/java/gov/ca/cwds/idm/dto/UsersSearchParameter.java
+++ src/main/java/gov/ca/cwds/idm/dto/UsersSearchParameter.java
@@ -1,8 +1,26 @@
 package gov.ca.cwds.idm.dto;
 
 public class UsersSearchParameter {
+  private String userCounty;
+  private String lastName;
   private Integer pageSize;
   private String email;
+
+  public String getUserCounty() {
+    return userCounty;
+  }
+
+  public void setUserCounty(String userCounty) {
+    this.userCounty = userCounty;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 
   public Integer getPageSize() {
     return pageSize;
@@ -21,6 +39,8 @@ public class UsersSearchParameter {
   }
 
   public static final class SearchParameterBuilder {
+    private String userCounty;
+    private String lastName;
     private Integer pageSize;
     private String email;
 
@@ -28,6 +48,16 @@ public class UsersSearchParameter {
 
     public static SearchParameterBuilder aSearchParameters() {
       return new SearchParameterBuilder();
+    }
+
+    public SearchParameterBuilder withUserCounty(String userCounty) {
+      this.userCounty = userCounty;
+      return this;
+    }
+
+    public SearchParameterBuilder withLastName(String lastName) {
+      this.lastName = lastName;
+      return this;
     }
 
     public SearchParameterBuilder withPageSize(Integer pageSize) {
@@ -40,8 +70,12 @@ public class UsersSearchParameter {
       return this;
     }
 
+
+
     public UsersSearchParameter build() {
       UsersSearchParameter usersSearchParameter = new UsersSearchParameter();
+      usersSearchParameter.setUserCounty(userCounty);
+      usersSearchParameter.setLastName(lastName);
       usersSearchParameter.setPageSize(pageSize);
       usersSearchParameter.setEmail(email);
       return usersSearchParameter;
