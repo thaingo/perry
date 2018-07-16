8c00823741fff6abeb99e4b0aa3d85745ef1af28
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-07-16 15:59:18 -0700

    Merge remote-tracking branch 'remotes/origin/master' into COG-236_Permissions_descriptions_dp

--- src/main/java/gov/ca/cwds/idm/IdmResource.java
+++ src/main/java/gov/ca/cwds/idm/IdmResource.java
@@ -4,15 +4,15 @@ import static gov.ca.cwds.service.messages.MessageCode.IDM_USER_VALIDATION_FAILE
 import static gov.ca.cwds.service.messages.MessageCode.USER_WITH_EMAIL_EXISTS_IN_IDM;
 
 import gov.ca.cwds.idm.dto.IdmApiCustomError;
-import gov.ca.cwds.idm.dto.UserUpdate;
 import gov.ca.cwds.idm.dto.User;
 import gov.ca.cwds.idm.dto.UserUpdate;
 import gov.ca.cwds.idm.dto.UserVerificationResult;
+import gov.ca.cwds.idm.persistence.model.Permission;
 import gov.ca.cwds.idm.service.DictionaryProvider;
 import gov.ca.cwds.idm.service.IdmService;
 import gov.ca.cwds.rest.api.domain.UserAlreadyExistsException;
-import gov.ca.cwds.rest.api.domain.UserNotFoundPerryException;
 import gov.ca.cwds.rest.api.domain.UserIdmValidationException;
+import gov.ca.cwds.rest.api.domain.UserNotFoundPerryException;
 import gov.ca.cwds.service.messages.MessageCode;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
