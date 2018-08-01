a3a278338939f5b097199506236e347dd8d8c131
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-08-01 14:35:41 -0700

    COG-383: restTemplate invocations are added

--- src/main/java/gov/ca/cwds/idm/service/ElasticSearchService.java
+++ src/main/java/gov/ca/cwds/idm/service/ElasticSearchService.java
@@ -1,6 +1,8 @@
 package gov.ca.cwds.idm.service;
 
 import gov.ca.cwds.idm.dto.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -13,22 +15,28 @@ public class ElasticSearchService {
   @Autowired
   private RestTemplate restTemplate;
 
-  @Autowired
-  private UserLogService userLogService;
-
   private static final String ES_CREATE_ENDPOINT = "/_create";
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ElasticSearchService.class);
 
   private String doraUrl = "http://localhost:8889/dora";
   private String esIndexName = "users";
   private String esTypeName = "user";
 
-
   public void createUser(User user) {
-
+    String id = user.getId();
+    id = "128e120c-d643-44ac-ad9b-4a3fc767f04d";//for mock
+    String url = getCreateUrl(id);
+    restTemplate.put(url, user);
+    LOGGER.info("User, username:{} was successfully inserted in Elastic Search index", id);
   }
 
   public void updateUser(User user) {
-
+    String id = user.getId();
+    id = "128e120c-d643-44ac-ad9b-4a3fc767f04d";//for mock
+    String url = getUpdateUrl(id);
+    restTemplate.put(url, user);
+    LOGGER.info("User, username:{} was successfully updated in Elastic Search index", id);
   }
 
   public void setDoraUrl(String doraUrl) {
