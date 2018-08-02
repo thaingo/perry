0a6cd26a84f906abe61c18d8f879138bbade7f62
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-08-01 17:18:38 -0700

    COG-383: ElasticSearchService is refactored

--- src/main/java/gov/ca/cwds/idm/service/ElasticSearchService.java
+++ src/main/java/gov/ca/cwds/idm/service/ElasticSearchService.java
@@ -1,6 +1,9 @@
 package gov.ca.cwds.idm.service;
 
 import gov.ca.cwds.idm.dto.User;
+import gov.ca.cwds.idm.persistence.model.OperationType;
+import java.util.HashMap;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,31 +15,49 @@ import org.springframework.web.client.RestTemplate;
 @Profile("idm")
 public class ElasticSearchService {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(ElasticSearchService.class);
+
+  private static final String CREATE_URL_TEMPLATE ="{doraUrl}/{esIndexName}/{esTypeName}/{id}/_create";
+  private static final String UPDATE_URL_TEMPLATE ="{doraUrl}/{esIndexName}/{esTypeName}/{id}";
+
   @Autowired
   private RestTemplate restTemplate;
-
-  private static final String ES_CREATE_ENDPOINT = "/_create";
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(ElasticSearchService.class);
 
   private String doraUrl = "http://localhost:8889/dora";
   private String esIndexName = "users";
   private String esTypeName = "user";
 
   public void createUser(User user) {
-    String id = user.getId();
-    id = "128e120c-d643-44ac-ad9b-4a3fc767f04d";//for mock
-    String url = getCreateUrl(id);
-    restTemplate.put(url, user);
-    LOGGER.info("User, username:{} was successfully inserted in Elastic Search index", id);
+    putUser(user, OperationType.CREATE);
+    LOGGER.info("User, username:{} was successfully inserted in Elastic Search index", user.getId());
   }
 
   public void updateUser(User user) {
+    putUser(user, OperationType.UPDATE);
+    LOGGER.info("User, username:{} was successfully updated in Elastic Search index", user.getId());
+  }
+
+  private void putUser(User user, OperationType operation) {
+
+    String urlTemplate;
+    if(operation == OperationType.CREATE) {
+      urlTemplate = CREATE_URL_TEMPLATE;
+    } else if(operation == OperationType.UPDATE){
+      urlTemplate = UPDATE_URL_TEMPLATE;
+    } else {
+      throw new IllegalArgumentException("Provided unsapported OperationType: " + operation.toString());
+    }
+
     String id = user.getId();
     id = "128e120c-d643-44ac-ad9b-4a3fc767f04d";//for mock
-    String url = getUpdateUrl(id);
-    restTemplate.put(url, user);
-    LOGGER.info("User, username:{} was successfully updated in Elastic Search index", id);
+
+    Map<String, String> params = new HashMap<>();
+    params.put("doraUrl", doraUrl);
+    params.put("esIndexName", esIndexName);
+    params.put("esTypeName", esTypeName);
+    params.put("id", id);
+
+    restTemplate.put(urlTemplate, user, params);
   }
 
   public void setDoraUrl(String doraUrl) {
@@ -49,13 +70,5 @@ public class ElasticSearchService {
 
   public void setEsTypeName(String esTypeName) {
     this.esTypeName = esTypeName;
-  }
-
-  String getUpdateUrl(String id) {
-    return doraUrl + '/' + esIndexName + '/' + esTypeName + '/' + id;
-  }
-
-  String getCreateUrl(String id) {
-    return getUpdateUrl(id) + ES_CREATE_ENDPOINT;
   }
 }
