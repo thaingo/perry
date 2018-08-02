1ec4c9776112a7d879e8b46482ed7bc7dbb2d472
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-08-02 10:35:59 -0700

    COG-383: ElasticSearchService properties are created

--- src/main/java/gov/ca/cwds/idm/service/ElasticSearchService.java
+++ src/main/java/gov/ca/cwds/idm/service/ElasticSearchService.java
@@ -2,6 +2,7 @@ package gov.ca.cwds.idm.service;
 
 import gov.ca.cwds.idm.dto.User;
 import gov.ca.cwds.idm.persistence.model.OperationType;
+import gov.ca.cwds.idm.service.cognito.ElasticSearchProperties;
 import java.util.HashMap;
 import java.util.Map;
 import org.slf4j.Logger;
@@ -17,15 +18,21 @@ public class ElasticSearchService {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(ElasticSearchService.class);
 
-  private static final String CREATE_URL_TEMPLATE ="{doraUrl}/{esIndexName}/{esTypeName}/{id}/_create";
-  private static final String UPDATE_URL_TEMPLATE ="{doraUrl}/{esIndexName}/{esTypeName}/{id}";
+  private static final String DORA_URL = "doraUrl";
+  private static final String ES_USER_INDEX = "esUserIndex";
+  private static final String ES_USER_TYPE = "esUserType";
+  private static final String ID = "id";
+
+  private static final String CREATE_URL_TEMPLATE =
+      "{" + DORA_URL + "}/{" + ES_USER_INDEX + "}/{" + ES_USER_TYPE + "}/{" + ID + "}/_create";
+
+  private static final String UPDATE_URL_TEMPLATE =
+      "{" + DORA_URL + "}/{" + ES_USER_INDEX + "}/{" + ES_USER_TYPE + "}/{" + ID + "}";
+
+  @Autowired private ElasticSearchProperties esProperties;
 
   @Autowired
   private RestTemplate restTemplate;
-
-  private String doraUrl = "http://localhost:8889/dora";
-  private String esIndexName = "users";
-  private String esTypeName = "user";
 
   public void createUser(User user) {
     putUser(user, OperationType.CREATE);
@@ -39,36 +46,31 @@ public class ElasticSearchService {
 
   private void putUser(User user, OperationType operation) {
 
-    String urlTemplate;
-    if(operation == OperationType.CREATE) {
-      urlTemplate = CREATE_URL_TEMPLATE;
-    } else if(operation == OperationType.UPDATE){
-      urlTemplate = UPDATE_URL_TEMPLATE;
-    } else {
-      throw new IllegalArgumentException("Provided unsapported OperationType: " + operation.toString());
-    }
+    String urlTemplate = getUrlTemplate(operation);
 
     String id = user.getId();
     id = "128e120c-d643-44ac-ad9b-4a3fc767f04d";//for mock
 
     Map<String, String> params = new HashMap<>();
-    params.put("doraUrl", doraUrl);
-    params.put("esIndexName", esIndexName);
-    params.put("esTypeName", esTypeName);
-    params.put("id", id);
+    params.put(DORA_URL, esProperties.getDoraUrl());
+    params.put(ES_USER_INDEX, esProperties.getIndex());
+    params.put(ES_USER_TYPE, esProperties.getType());
+    params.put(ID, id);
 
     restTemplate.put(urlTemplate, user, params);
   }
 
-  public void setDoraUrl(String doraUrl) {
-    this.doraUrl = doraUrl;
+  private String getUrlTemplate(OperationType operation){
+    if(operation == OperationType.CREATE) {
+      return CREATE_URL_TEMPLATE;
+    } else if(operation == OperationType.UPDATE){
+      return UPDATE_URL_TEMPLATE;
+    } else {
+      throw new IllegalArgumentException("Provided unsupported OperationType: " + operation.toString());
+    }
   }
 
-  public void setEsIndexName(String esIndexName) {
-    this.esIndexName = esIndexName;
-  }
-
-  public void setEsTypeName(String esTypeName) {
-    this.esTypeName = esTypeName;
+  public void setElasticSearchProperties(ElasticSearchProperties esProperties) {
+    this.esProperties = esProperties;
   }
 }
