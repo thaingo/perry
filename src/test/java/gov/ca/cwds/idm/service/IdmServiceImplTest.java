b6b39f8bd2ffea08b10eadcb8b6df1fc49b88489
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-08-16 16:07:06 -0700

    COG-446: cases for userCreate() PartialSuccessExceptions are added

--- src/test/java/gov/ca/cwds/idm/service/IdmServiceImplTest.java
+++ src/test/java/gov/ca/cwds/idm/service/IdmServiceImplTest.java
@@ -4,9 +4,13 @@ import static gov.ca.cwds.idm.service.IdmServiceImpl.transformSearchValues;
 import static gov.ca.cwds.idm.service.cognito.StandardUserAttribute.EMAIL;
 import static gov.ca.cwds.idm.service.cognito.StandardUserAttribute.FIRST_NAME;
 import static gov.ca.cwds.idm.service.cognito.StandardUserAttribute.RACFID_STANDARD;
+import static gov.ca.cwds.service.messages.MessageCode.USER_CREATE_SAVE_TO_SEARCH_AND_DB_LOG_ERRORS;
+import static gov.ca.cwds.service.messages.MessageCode.USER_CREATE_SAVE_TO_SEARCH_ERROR;
 import static gov.ca.cwds.util.Utils.toSet;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -15,9 +19,12 @@ import gov.ca.cwds.idm.IdmResourceTest;
 import gov.ca.cwds.idm.WithMockCustomUser;
 import gov.ca.cwds.idm.dto.User;
 import gov.ca.cwds.idm.persistence.UserLogRepository;
+import gov.ca.cwds.idm.persistence.model.UserLog;
 import gov.ca.cwds.idm.service.cognito.CognitoServiceFacade;
 import gov.ca.cwds.idm.service.cognito.util.CognitoUtils;
+import gov.ca.cwds.rest.api.domain.PartialSuccessException;
 import gov.ca.cwds.service.CwsUserInfoService;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -62,8 +69,57 @@ public class IdmServiceImplTest {
   public void testCreateUserSuccess() {
     User user = user();
     setCreateUserResult(user, NEW_USER_SUCCESS_ID);
+
     String id = service.createUser(user);
     assertThat(id, is(NEW_USER_SUCCESS_ID));
+  }
+
+  @Test
+  @WithMockCustomUser
+  public void testCreateUser_SearchFail() {
+    User user = user();
+    setCreateUserResult(user, NEW_USER_SUCCESS_ID);
+
+    Exception doraError = new RuntimeException("Dora error");
+    when(searchServiceMock.createUser(any(User.class))).thenThrow(doraError);
+
+    try {
+      service.createUser(user);
+      fail("should throw PartialSuccessException");
+    } catch (PartialSuccessException e) {
+      assertThat(e.getUserId(), is(NEW_USER_SUCCESS_ID));
+      assertThat(e.getErrorCode(), is(USER_CREATE_SAVE_TO_SEARCH_ERROR));
+
+      List<Exception> causes = e.getCauses();
+      assertThat(causes.size(), is(1));
+      assertThat(causes.get(0), is(doraError));
+    }
+  }
+
+  @Test
+  @WithMockCustomUser
+  public void testCreateUser_SearchAndDbLogFail() {
+    User user = user();
+    setCreateUserResult(user, NEW_USER_SUCCESS_ID);
+
+    Exception doraError = new RuntimeException("Dora error");
+    when(searchServiceMock.createUser(any(User.class))).thenThrow(doraError);
+
+    Exception dbError = new RuntimeException("DB error");
+    when(userLogRepositoryMock.save(any(UserLog.class))).thenThrow(dbError);
+
+    try {
+      service.createUser(user);
+      fail("should throw PartialSuccessException");
+    } catch (PartialSuccessException e) {
+      assertThat(e.getUserId(), is(NEW_USER_SUCCESS_ID));
+      assertThat(e.getErrorCode(), is(USER_CREATE_SAVE_TO_SEARCH_AND_DB_LOG_ERRORS));
+
+      List<Exception> causes = e.getCauses();
+      assertThat(causes.size(), is(2));
+      assertThat(causes.get(0), is(doraError));
+      assertThat(causes.get(1), is(dbError));
+    }
   }
 
   @Test
