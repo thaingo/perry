e970ba230668bea4d6d33a43d9f7a6d735d38c7e
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-09-11 14:36:51 -0700

    COG-541: PerryIdmAdminConfiguration is restored

---
+++ src/main/java/gov/ca/cwds/config/api/idm/PerryIdmAdminConfiguration.java
@@ -0,0 +1,29 @@
+package gov.ca.cwds.config.api.idm;
+
+import static gov.ca.cwds.config.api.idm.Roles.COUNTY_ADMIN;
+import static gov.ca.cwds.config.api.idm.Roles.CWS_ADMIN;
+import static gov.ca.cwds.config.api.idm.Roles.OFFICE_ADMIN;
+import static gov.ca.cwds.config.api.idm.Roles.STATE_ADMIN;
+
+import gov.ca.cwds.config.api.common.BaseApiConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@Order(2)
+@Profile("idm")
+@EnableWebSecurity
+public class PerryIdmAdminConfiguration extends BaseApiConfiguration {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.antMatcher("/idm/permissions/**")
+        .authorizeRequests()
+        .anyRequest()
+        .hasAnyAuthority(CWS_ADMIN, COUNTY_ADMIN, STATE_ADMIN, OFFICE_ADMIN);
+    super.configure(http);
+  }
+}
