075e4b44da1348098a9d394bb45e7b16f590f4f6
Alexander Serbin <alserbin@gmail.com>
2018-09-12 19:25:57 -0700

    COG-557 fixed sonar issue

--- src/test/java/gov/ca/cwds/BaseIntegrationTest.java
+++ src/test/java/gov/ca/cwds/BaseIntegrationTest.java
@@ -12,7 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-public class BaseIntegrationTest {
+public abstract class BaseIntegrationTest {
 
   protected MockMvc mockMvc;
 
