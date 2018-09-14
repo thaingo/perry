0f0ef0d23ae1d5ef37253f48a00b36f004639be5
Alexander Serbin <alserbin@gmail.com>
2018-09-13 19:25:40 -0700

    COG-532 refactoring: extracted interface for cognito facade, interface introduced improves code readability. Package-private visibility for facade implementation hides details of implementation for other packages.

--- src/main/java/gov/ca/cwds/idm/service/cognito/CognitoServiceFacade.java
+++ src/main/java/gov/ca/cwds/idm/service/cognito/CognitoServiceFacade.java
@@ -1,294 +1,51 @@
 package gov.ca.cwds.idm.service.cognito;
 
-import static gov.ca.cwds.idm.persistence.ns.OperationType.GET;
-import static gov.ca.cwds.idm.persistence.ns.OperationType.UPDATE;
-import static gov.ca.cwds.idm.service.cognito.util.CognitoUtils.EMAIL_DELIVERY;
-import static gov.ca.cwds.idm.service.cognito.util.CognitoUtils.buildCreateUserAttributes;
-import static gov.ca.cwds.idm.service.cognito.util.CognitoUtils.createPermissionsAttribute;
-import static gov.ca.cwds.service.messages.MessageCode.ERROR_CONNECT_TO_IDM;
-import static gov.ca.cwds.service.messages.MessageCode.ERROR_GET_USER_FROM_IDM;
-import static gov.ca.cwds.service.messages.MessageCode.ERROR_UPDATE_USER_IN_IDM;
-import static gov.ca.cwds.service.messages.MessageCode.IDM_USER_VALIDATION_FAILED;
-import static gov.ca.cwds.service.messages.MessageCode.UNABLE_CREATE_NEW_IDM_USER;
-import static gov.ca.cwds.service.messages.MessageCode.USER_NOT_FOUND_BY_ID_IN_IDM;
-import static gov.ca.cwds.service.messages.MessageCode.USER_WITH_EMAIL_EXISTS_IN_IDM;
-import static gov.ca.cwds.util.Utils.toLowerCase;
-
-import com.amazonaws.AmazonWebServiceRequest;
-import com.amazonaws.AmazonWebServiceResult;
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
-import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
 import com.amazonaws.services.cognitoidp.model.AdminCreateUserRequest;
-import com.amazonaws.services.cognitoidp.model.AdminCreateUserResult;
-import com.amazonaws.services.cognitoidp.model.AdminDisableUserRequest;
-import com.amazonaws.services.cognitoidp.model.AdminEnableUserRequest;
 import com.amazonaws.services.cognitoidp.model.AdminGetUserRequest;
-import com.amazonaws.services.cognitoidp.model.AdminGetUserResult;
-import com.amazonaws.services.cognitoidp.model.AdminUpdateUserAttributesRequest;
-import com.amazonaws.services.cognitoidp.model.AttributeType;
-import com.amazonaws.services.cognitoidp.model.DescribeUserPoolRequest;
-import com.amazonaws.services.cognitoidp.model.InvalidParameterException;
 import com.amazonaws.services.cognitoidp.model.ListUsersRequest;
-import com.amazonaws.services.cognitoidp.model.ListUsersResult;
-import com.amazonaws.services.cognitoidp.model.UserNotFoundException;
 import com.amazonaws.services.cognitoidp.model.UserType;
-import com.amazonaws.services.cognitoidp.model.UsernameExistsException;
 import gov.ca.cwds.idm.dto.User;
 import gov.ca.cwds.idm.dto.UserEnableStatusRequest;
 import gov.ca.cwds.idm.dto.UserUpdate;
-import gov.ca.cwds.idm.persistence.ns.OperationType;
 import gov.ca.cwds.idm.service.cognito.dto.CognitoUserPage;
 import gov.ca.cwds.idm.service.cognito.dto.CognitoUsersSearchCriteria;
-import gov.ca.cwds.idm.service.cognito.util.CognitoUtils;
-import gov.ca.cwds.rest.api.domain.PerryException;
-import gov.ca.cwds.rest.api.domain.UserAlreadyExistsException;
-import gov.ca.cwds.rest.api.domain.UserIdmValidationException;
-import gov.ca.cwds.rest.api.domain.UserNotFoundPerryException;
-import gov.ca.cwds.service.messages.MessagesService;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.function.Function;
-import javax.annotation.PostConstruct;
-import liquibase.util.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
 
-@Service(value = "cognitoServiceFacade")
+/**
+ * Created by Alexander Serbin on 9/13/2018
+ */
 @Profile("idm")
-@SuppressWarnings({"fb-contrib:EXS_EXCEPTION_SOFTENING_NO_CONSTRAINTS"})
-public class CognitoServiceFacade {
-  private static final Logger LOGGER = LoggerFactory.getLogger(CognitoServiceFacade.class);
+public interface CognitoServiceFacade {
 
-  @Autowired private CognitoProperties properties;
+  UserType createUser(User user);
 
-  @Autowired private MessagesService messages;
+  AdminCreateUserRequest createAdminCreateUserRequest(User user);
 
-  private AWSCognitoIdentityProvider identityProvider;
+  CognitoUserPage searchPage(CognitoUsersSearchCriteria searchCriteria);
 
-  @PostConstruct
-  public void init() {
-    AWSCredentialsProvider credentialsProvider =
-        new AWSStaticCredentialsProvider(
-            new BasicAWSCredentials(properties.getIamAccessKeyId(), properties.getIamSecretKey()));
-    identityProvider =
-        AWSCognitoIdentityProviderClientBuilder.standard()
-            .withCredentials(credentialsProvider)
-            .withRegion(Regions.fromName(properties.getRegion()))
-            .build();
-  }
+  List<UserType> searchAllPages(CognitoUsersSearchCriteria searchCriteria);
 
-  public UserType createUser(User user) {
+  UserType getCognitoUserById(String id);
 
-    AdminCreateUserRequest request = createAdminCreateUserRequest(user);
+  AdminGetUserRequest createAdminGetUserRequest(String id);
 
-    try {
-      AdminCreateUserResult result = identityProvider.adminCreateUser(request);
-      return result.getUser();
-
-    } catch (UsernameExistsException e) {
-      String causeMsg = messages.get(USER_WITH_EMAIL_EXISTS_IN_IDM, user.getEmail());
-      String msg = messages.get(UNABLE_CREATE_NEW_IDM_USER, causeMsg);
-      LOGGER.error(msg, e);
-      throw new UserAlreadyExistsException(causeMsg, e);
-
-    } catch (InvalidParameterException e) {
-      String msg = messages.get(IDM_USER_VALIDATION_FAILED);
-      LOGGER.error(msg, e);
-      throw new UserIdmValidationException(msg, e);
-    }
-  }
+  ListUsersRequest composeListUsersRequest(CognitoUsersSearchCriteria criteria);
 
   //method is used in annotation, don't remove it
-  public String getCountyName(String userId) {
-    return CognitoUtils.getCountyName(getCognitoUserById(userId));
-  }
+  String getCountyName(String userId);
 
-   public AdminCreateUserRequest createAdminCreateUserRequest(User user) {
-
-    user.setEmail(toLowerCase(user.getEmail()));
-
-    return
-        new AdminCreateUserRequest()
-            .withUsername(user.getEmail())
-            .withUserPoolId(properties.getUserpool())
-            .withDesiredDeliveryMediums(EMAIL_DELIVERY)
-            .withUserAttributes(buildCreateUserAttributes(user));
-  }
-
-  public CognitoUserPage searchPage(CognitoUsersSearchCriteria searchCriteria) {
-    ListUsersRequest request = composeListUsersRequest(searchCriteria);
-    try {
-      ListUsersResult result = identityProvider.listUsers(request);
-      return new CognitoUserPage(result.getUsers(), result.getPaginationToken());
-    } catch (Exception e) {
-      throw new PerryException(messages.get(ERROR_CONNECT_TO_IDM), e);
-    }
-  }
-
-  public List<UserType> searchAllPages(CognitoUsersSearchCriteria searchCriteria) {
-    List<UserType> result = new ArrayList<>();
-    addPage(result, searchCriteria);
-    return result;
-  }
-
-  private void addPage(List<UserType> result, CognitoUsersSearchCriteria searchCriteria) {
-    CognitoUserPage userPage = searchPage(searchCriteria);
-    result.addAll(userPage.getUsers());
-    String paginationToken = userPage.getPaginationToken();
-
-    if (StringUtils.isNotEmpty(paginationToken)) {
-      CognitoUsersSearchCriteria searchCriteria2 = new CognitoUsersSearchCriteria(searchCriteria);
-      searchCriteria2.setPaginationToken(paginationToken);
-      addPage(result, searchCriteria2);
-    }
-  }
-
-  public void healthCheck() {
-    identityProvider.describeUserPool(
-        new DescribeUserPoolRequest().withUserPoolId(properties.getUserpool()));
-  }
-
-  public UserType getCognitoUserById(String id) {
-    AdminGetUserRequest request = createAdminGetUserRequest(id);
-    AdminGetUserResult agur;
-
-    agur = executeInCognito(identityProvider::adminGetUser, request, id, GET);
-
-    return new UserType()
-        .withUsername(agur.getUsername())
-        .withAttributes(agur.getUserAttributes())
-        .withEnabled(agur.getEnabled())
-        .withUserCreateDate(agur.getUserCreateDate())
-        .withUserLastModifiedDate(agur.getUserLastModifiedDate())
-        .withUserStatus(agur.getUserStatus());
-  }
-
-  public AdminGetUserRequest createAdminGetUserRequest(String id) {
-    return new AdminGetUserRequest().withUsername(id).withUserPoolId(properties.getUserpool());
-  }
+  void healthCheck();
 
   /**
    @return true if Cognito operations were really executed, false otherwise
    */
-  public boolean updateUserAttributes(
-      String id, UserType existedCognitoUser, UserUpdate updateUserDto) {
-
-    boolean executed = false;
-
-    List<AttributeType> updateAttributes = getUpdateAttributes(existedCognitoUser, updateUserDto);
-
-    if (!updateAttributes.isEmpty()) {
-      AdminUpdateUserAttributesRequest adminUpdateUserAttributesRequest =
-          new AdminUpdateUserAttributesRequest()
-              .withUsername(id)
-              .withUserPoolId(properties.getUserpool())
-              .withUserAttributes(updateAttributes);
-
-      executeInCognito(
-          identityProvider::adminUpdateUserAttributes, adminUpdateUserAttributesRequest, id, UPDATE);
-      executed = true;
-    }
-    return executed;
-  }
-
-  private List<AttributeType> getUpdateAttributes(
-      UserType existedCognitoUser, UserUpdate updateUserDto) {
-    List<AttributeType> updateAttributes = new ArrayList<>();
-
-    Set<String> existedUserPermissions = CognitoUtils.getPermissions(existedCognitoUser);
-    Set<String> newUserPermissions = updateUserDto.getPermissions();
-
-    if (newUserPermissions != null && !newUserPermissions.equals(existedUserPermissions)) {
-      AttributeType permissionsAttr = createPermissionsAttribute(newUserPermissions);
-      updateAttributes.add(permissionsAttr);
-    }
-
-    return updateAttributes;
-  }
+  boolean updateUserAttributes(
+      String id, UserType existedCognitoUser, UserUpdate updateUserDto);
 
   /**
    @return true if Cognito operations were really executed, false otherwise
    */
-  public boolean changeUserEnabledStatus(UserEnableStatusRequest request) {
-    boolean executed = false;
+  boolean changeUserEnabledStatus(UserEnableStatusRequest request);
 
-    String id = request.getUserId();
-    Boolean existedEnabled = request.getExistedEnabled();
-    Boolean newEnabled = request.getNewEnabled();
-
-    if (newEnabled != null && !newEnabled.equals(existedEnabled)) {
-      if (newEnabled) {
-        AdminEnableUserRequest adminEnableUserRequest =
-            new AdminEnableUserRequest().withUsername(id).withUserPoolId(properties.getUserpool());
-        executeInCognito(identityProvider::adminEnableUser, adminEnableUserRequest, id, UPDATE);
-        executed =  true;
-
-      } else {
-        AdminDisableUserRequest adminDisableUserRequest =
-            new AdminDisableUserRequest().withUsername(id).withUserPoolId(properties.getUserpool());
-        executeInCognito(identityProvider::adminDisableUser, adminDisableUserRequest, id, UPDATE);
-        executed =  true;
-      }
-    }
-    return executed;
-  }
-
-  public ListUsersRequest composeListUsersRequest(CognitoUsersSearchCriteria criteria) {
-    ListUsersRequest request = new ListUsersRequest().withUserPoolId(properties.getUserpool());
-    if (criteria.getPageSize() != null) {
-      request = request.withLimit(criteria.getPageSize());
-    }
-    if (criteria.getPaginationToken() != null) {
-      request = request.withPaginationToken(criteria.getPaginationToken());
-    }
-    if (criteria.getSearchAttrName() != null) {
-      request = request.withFilter(criteria.getSearchAttrName() + " = \"" + criteria.getSearchAttrValue() + "\"");
-    }
-    return request;
-  }
-
-  public void setProperties(CognitoProperties properties) {
-    this.properties = properties;
-  }
-
-  public AWSCognitoIdentityProvider getIdentityProvider() {
-    return identityProvider;
-  }
-
-  public void setIdentityProvider(AWSCognitoIdentityProvider identityProvider) {
-    this.identityProvider = identityProvider;
-  }
-  public void setMessagesService(MessagesService messages) {
-    this.messages = messages;
-  }
-
-  private <T extends AmazonWebServiceRequest, R extends AmazonWebServiceResult>
-    R executeInCognito(Function<T, R> function, T request, String userId, OperationType operation) {
-
-    try {
-      return function.apply(request);
-    } catch (UserNotFoundException e) {
-      String msg = messages.get(USER_NOT_FOUND_BY_ID_IN_IDM, userId);
-      LOGGER.error(msg, e);
-      throw new UserNotFoundPerryException(msg, e);
-
-    } catch (Exception e) {
-      String msg = "";
-      if(operation == UPDATE) {
-        msg = messages.get(ERROR_UPDATE_USER_IN_IDM);
-      } else if(operation == GET) {
-        msg = messages.get(ERROR_GET_USER_FROM_IDM);
-      }
-      LOGGER.error(msg, e);
-      throw new PerryException(msg, e);
-    }
-  }
 }
---
+++ src/test/java/gov/ca/cwds/idm/service/cognito/TestCognitoServiceFacade.java
@@ -0,0 +1,432 @@
+package gov.ca.cwds.idm.service.cognito;
+
+import static gov.ca.cwds.Constants.ABSENT_USER_ID;
+import static gov.ca.cwds.Constants.ERROR_USER_ID;
+import static gov.ca.cwds.Constants.ES_ERROR_CREATE_USER_EMAIL;
+import static gov.ca.cwds.Constants.NEW_USER_ES_FAIL_ID;
+import static gov.ca.cwds.Constants.NEW_USER_SUCCESS_ID;
+import static gov.ca.cwds.Constants.SOME_PAGINATION_TOKEN;
+import static gov.ca.cwds.Constants.USERPOOL;
+import static gov.ca.cwds.Constants.USER_NO_RACFID_ID;
+import static gov.ca.cwds.Constants.USER_WITH_NO_PHONE_EXTENSION;
+import static gov.ca.cwds.Constants.USER_WITH_RACFID_AND_DB_DATA_ID;
+import static gov.ca.cwds.Constants.USER_WITH_RACFID_ID;
+import static gov.ca.cwds.idm.service.cognito.CustomUserAttribute.COUNTY;
+import static gov.ca.cwds.idm.service.cognito.CustomUserAttribute.PERMISSIONS;
+import static gov.ca.cwds.idm.service.cognito.CustomUserAttribute.RACFID_CUSTOM;
+import static gov.ca.cwds.idm.service.cognito.CustomUserAttribute.RACFID_CUSTOM_2;
+import static gov.ca.cwds.idm.service.cognito.CustomUserAttribute.ROLES;
+import static gov.ca.cwds.idm.service.cognito.StandardUserAttribute.EMAIL;
+import static gov.ca.cwds.idm.service.cognito.StandardUserAttribute.FIRST_NAME;
+import static gov.ca.cwds.idm.service.cognito.StandardUserAttribute.LAST_NAME;
+import static gov.ca.cwds.idm.service.cognito.StandardUserAttribute.RACFID_STANDARD;
+import static gov.ca.cwds.idm.service.cognito.util.CognitoUsersSearchCriteriaUtil.DEFAULT_PAGESIZE;
+import static gov.ca.cwds.idm.service.cognito.util.CognitoUsersSearchCriteriaUtil.composeToGetFirstPageByAttribute;
+import static gov.ca.cwds.idm.util.TestUtils.attr;
+import static gov.ca.cwds.idm.util.TestUtils.date;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
+import com.amazonaws.services.cognitoidp.model.AdminGetUserRequest;
+import com.amazonaws.services.cognitoidp.model.AdminGetUserResult;
+import com.amazonaws.services.cognitoidp.model.AttributeType;
+import com.amazonaws.services.cognitoidp.model.InternalErrorException;
+import com.amazonaws.services.cognitoidp.model.ListUsersRequest;
+import com.amazonaws.services.cognitoidp.model.ListUsersResult;
+import com.amazonaws.services.cognitoidp.model.UserNotFoundException;
+import com.amazonaws.services.cognitoidp.model.UserType;
+import gov.ca.cwds.idm.WithMockCustomUser;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.annotation.PostConstruct;
+import liquibase.util.StringUtils;
+
+public class TestCognitoServiceFacade extends CognitoServiceFacadeImpl {
+
+  private AWSCognitoIdentityProvider cognito;
+
+  @PostConstruct
+  @Override
+  public void init() {
+    cognito = mock(AWSCognitoIdentityProvider.class);
+
+    CognitoProperties properties = new CognitoProperties();
+    properties.setIamAccessKeyId("iamAccessKeyId");
+    properties.setIamSecretKey("iamSecretKey");
+    properties.setUserpool(USERPOOL);
+    properties.setRegion("us-east-2");
+
+    setProperties(properties);
+    setIdentityProvider(cognito);
+
+    TestUser userWithoutRacfid =
+        testUser(
+            USER_NO_RACFID_ID,
+            Boolean.TRUE,
+            "FORCE_CHANGE_PASSWORD",
+            date(2018, 5, 4),
+            date(2018, 5, 30),
+            "donzano@gmail.com",
+            "Don",
+            "Manzano",
+            WithMockCustomUser.COUNTY,
+            "RFA-rollout:Snapshot-rollout:",
+            "CWS-worker:County-admin",
+            null);
+
+    TestUser userWithRacfid =
+        testUser(
+            USER_WITH_RACFID_ID,
+            Boolean.TRUE,
+            "CONFIRMED",
+            date(2018, 5, 4),
+            date(2018, 5, 29),
+            "julio@gmail.com",
+            "Julio",
+            "Iglecias",
+            WithMockCustomUser.COUNTY,
+            "Hotline-rollout",
+            "CWS-worker:County-admin",
+            "YOLOD");
+
+    TestUser userWithRacfidAndDbData =
+        testUser(
+            USER_WITH_RACFID_AND_DB_DATA_ID,
+            Boolean.TRUE,
+            "CONFIRMED",
+            date(2018, 5, 3),
+            date(2018, 5, 31),
+            "garcia@gmail.com",
+            "Garcia",
+            "Gonzales",
+            WithMockCustomUser.COUNTY,
+            "test",
+            null,
+            "SMITHBO");
+
+    TestUser userWithNoPhoneExtension =
+        testUser(
+            USER_WITH_NO_PHONE_EXTENSION,
+            Boolean.TRUE,
+            "CONFIRMED",
+            date(2018, 5, 3),
+            date(2018, 5, 31),
+            "gabriel@gmail.com",
+            "Gabriel",
+            "Huanito",
+            WithMockCustomUser.COUNTY,
+            "test",
+            null,
+            "SMITHB2");
+
+    TestUser newSuccessUser =
+        testUser(
+            NEW_USER_SUCCESS_ID,
+            Boolean.TRUE,
+            "FORCE_CHANGE_PASSWORD",
+            date(2018, 5, 4),
+            date(2018, 5, 30),
+            "gonzales@gmail.com",
+            "Garcia",
+            "Gonzales",
+            WithMockCustomUser.COUNTY,
+            null,
+            null,
+            null);
+
+    TestUser doraFailUser =
+        testUser(
+            NEW_USER_ES_FAIL_ID,
+            Boolean.TRUE,
+            "FORCE_CHANGE_PASSWORD",
+            date(2018, 5, 4),
+            date(2018, 5, 30),
+            ES_ERROR_CREATE_USER_EMAIL,
+            "Garcia",
+            "Gonzales",
+            WithMockCustomUser.COUNTY,
+            null,
+            null,
+            null);
+
+    setUpGetAbsentUserRequestAndResult();
+
+    setUpGetErrorUserRequestAndResult();
+
+    setListUsersRequestAndResult("", userWithoutRacfid, userWithRacfid, userWithRacfidAndDbData);
+
+    setListUsersRequestAndResult(SOME_PAGINATION_TOKEN, userWithoutRacfid);
+
+    setSearchUsersByEmailRequestAndResult("julio@gmail.com", "test@test.com", userWithRacfid);
+
+    setSearchByRacfidRequestAndResult(userWithRacfid);
+
+    setSearchByRacfidRequestAndResult(userWithRacfidAndDbData);
+
+    setSearchByRacfidRequestAndResult(userWithNoPhoneExtension);
+  }
+
+  private void setListUsersRequestAndResult(String paginationToken, TestUser... testUsers) {
+    ListUsersRequest request =
+        new ListUsersRequest().withUserPoolId(USERPOOL).withLimit(DEFAULT_PAGESIZE);
+
+    if (StringUtils.isNotEmpty(paginationToken)) {
+      request.withPaginationToken(paginationToken);
+    }
+
+    List<UserType> userTypes =
+        Arrays.stream(testUsers)
+            .map(TestCognitoServiceFacade::userType)
+            .collect(Collectors.toList());
+
+    ListUsersResult result = new ListUsersResult().withUsers(userTypes);
+
+    when(cognito.listUsers(request)).thenReturn(result);
+  }
+
+  private TestUser testUser(
+      String id,
+      Boolean enabled,
+      String status,
+      Date userCreateDate,
+      Date lastModifiedDate,
+      String email,
+      String firstName,
+      String lastName,
+      String county,
+      String permissions,
+      String roles,
+      String racfId) {
+
+    TestUser testUser =
+        new TestUser(
+            id,
+            enabled,
+            status,
+            userCreateDate,
+            lastModifiedDate,
+            email,
+            firstName,
+            lastName,
+            county,
+            permissions,
+            roles,
+            racfId);
+
+    setUpGetUserRequestAndResult(testUser);
+
+    return testUser;
+  }
+
+  private static Collection<AttributeType> attrs(TestUser testUser) {
+    Collection<AttributeType> attrs = new ArrayList<>();
+
+    if (testUser.getEmail() != null) {
+      attrs.add(attr(EMAIL.getName(), testUser.getEmail()));
+    }
+    if (testUser.getFirstName() != null) {
+      attrs.add(attr(FIRST_NAME.getName(), testUser.getFirstName()));
+    }
+    if (testUser.getLastName() != null) {
+      attrs.add(attr(LAST_NAME.getName(), testUser.getLastName()));
+    }
+    if (testUser.getCounty() != null) {
+      attrs.add(attr(COUNTY.getName(), testUser.getCounty()));
+    }
+    if (testUser.getPermissions() != null) {
+      attrs.add(attr(PERMISSIONS.getName(), testUser.getPermissions()));
+    }
+    if (testUser.getRoles() != null) {
+      attrs.add(attr(ROLES.getName(), testUser.getRoles()));
+    }
+    if (testUser.getRacfId() != null) {
+      attrs.add(attr(RACFID_CUSTOM.getName(), testUser.getRacfId()));
+      attrs.add(attr(RACFID_STANDARD.getName(), testUser.getRacfId()));
+      attrs.add(attr(RACFID_CUSTOM_2.getName(), testUser.getRacfId()));
+    }
+    return attrs;
+  }
+
+  private static UserType userType(TestUser testUser) {
+    UserType userType =
+        new UserType()
+            .withUsername(testUser.getId())
+            .withEnabled(testUser.getEnabled())
+            .withUserCreateDate(testUser.getUserCreateDate())
+            .withUserLastModifiedDate(testUser.getLastModifiedDate())
+            .withUserStatus(testUser.getStatus());
+
+    userType.withAttributes(attrs(testUser));
+    return userType;
+  }
+
+  private void setSearchUsersByEmailRequestAndResult(
+      String email_correct, String email_wrong, TestUser... testUsers) {
+    ListUsersRequest request_correct =
+        new ListUsersRequest()
+            .withUserPoolId(USERPOOL)
+            .withLimit(DEFAULT_PAGESIZE)
+            .withFilter("email = \"" + email_correct + "\"");
+
+    ListUsersRequest request_wrong =
+        new ListUsersRequest()
+            .withUserPoolId(USERPOOL)
+            .withLimit(DEFAULT_PAGESIZE)
+            .withFilter("email = \"" + email_wrong + "\"");
+
+    List<UserType> userTypes =
+        Arrays.stream(testUsers)
+            .map(TestCognitoServiceFacade::userType)
+            .collect(Collectors.toList());
+
+    ListUsersResult result = new ListUsersResult().withUsers(userTypes);
+    ListUsersResult result_empty = new ListUsersResult();
+
+    when(cognito.listUsers(request_correct)).thenReturn(result);
+    when(cognito.listUsers(request_wrong)).thenReturn(result_empty);
+  }
+
+  private void setUpGetUserRequestAndResult(TestUser testUser) {
+
+    AdminGetUserRequest getUserRequest =
+        new AdminGetUserRequest().withUsername(testUser.getId()).withUserPoolId(USERPOOL);
+
+    AdminGetUserResult getUserResult = new AdminGetUserResult();
+    getUserResult.setUsername(testUser.getId());
+    getUserResult.setEnabled(testUser.getEnabled());
+    getUserResult.setUserStatus(testUser.getStatus());
+    getUserResult.setUserCreateDate(testUser.getUserCreateDate());
+    getUserResult.setUserLastModifiedDate(testUser.getLastModifiedDate());
+
+    getUserResult.withUserAttributes(attrs(testUser));
+
+    when(cognito.adminGetUser(getUserRequest)).thenReturn(getUserResult);
+  }
+
+  private void setUpGetAbsentUserRequestAndResult() {
+
+    AdminGetUserRequest getUserRequest =
+        new AdminGetUserRequest().withUsername(ABSENT_USER_ID).withUserPoolId(USERPOOL);
+
+    when(cognito.adminGetUser(getUserRequest))
+        .thenThrow(new UserNotFoundException("user not found"));
+  }
+
+  private void setUpGetErrorUserRequestAndResult() {
+
+    AdminGetUserRequest getUserRequest =
+        new AdminGetUserRequest().withUsername(ERROR_USER_ID).withUserPoolId(USERPOOL);
+
+    when(cognito.adminGetUser(getUserRequest))
+        .thenThrow(new InternalErrorException("internal error"));
+  }
+
+  ListUsersRequest setSearchByRacfidRequestAndResult(TestUser testUser){
+
+    ListUsersRequest request =
+        composeListUsersRequest(composeToGetFirstPageByAttribute(RACFID_STANDARD, testUser.getRacfId()));
+
+    ListUsersResult result = new ListUsersResult().withUsers(userType(testUser));
+
+    when(cognito.listUsers(request)).thenReturn(result);
+
+    return request;
+  }
+
+  static class TestUser {
+
+    private String id;
+    private Boolean enabled;
+    private String status;
+    private Date userCreateDate;
+    private Date lastModifiedDate;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String county;
+    private String permissions;
+    private String roles;
+    private String racfId;
+
+    TestUser(
+        String id,
+        Boolean enabled,
+        String status,
+        Date userCreateDate,
+        Date lastModifiedDate,
+        String email,
+        String firstName,
+        String lastName,
+        String county,
+        String permissions,
+        String roles,
+        String racfId) {
+      this.id = id;
+      this.enabled = enabled;
+      this.status = status;
+      this.userCreateDate = userCreateDate;
+      this.lastModifiedDate = lastModifiedDate;
+      this.email = email;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.county = county;
+      this.permissions = permissions;
+      this.roles = roles;
+      this.racfId = racfId;
+    }
+
+    public String getId() {
+      return id;
+    }
+
+    public Boolean getEnabled() {
+      return enabled;
+    }
+
+    public String getStatus() {
+      return status;
+    }
+
+    public Date getUserCreateDate() {
+      return userCreateDate;
+    }
+
+    public Date getLastModifiedDate() {
+      return lastModifiedDate;
+    }
+
+    public String getEmail() {
+      return email;
+    }
+
+    public String getFirstName() {
+      return firstName;
+    }
+
+    public String getLastName() {
+      return lastName;
+    }
+
+    public String getCounty() {
+      return county;
+    }
+
+    public String getPermissions() {
+      return permissions;
+    }
+
+    public String getRoles() {
+      return roles;
+    }
+
+    public String getRacfId() {
+      return racfId;
+    }
+  }
+
+}
