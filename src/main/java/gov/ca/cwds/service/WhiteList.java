2dcdf809642752074494a8d7b5ce9d6492473e4e
Volodymyr Petrusha <volodymyr.petrusha@osi.ca.gov>
2018-09-21 14:47:18 -0700

    CANS-385 Perry: support wildcards in callback URLs white list
    AntPathMatcher was applied for WhiteList

--- src/main/java/gov/ca/cwds/service/WhiteList.java
+++ src/main/java/gov/ca/cwds/service/WhiteList.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import org.springframework.util.AntPathMatcher;
 
 /**
  * Created by dmitry.rudenko on 9/18/2017.
@@ -15,9 +16,11 @@ public class WhiteList {
 
   PerryProperties configuration;
 
+  private AntPathMatcher pathMatcher = new AntPathMatcher();
+
   public void validate(String param, String url) {
-    if (!disabled(configuration.getWhiteList())
-        && !(configuration.getWhiteList().contains(url)
+    List<String> patterns = configuration.getWhiteList();
+    if (!disabled(patterns) && !(anyMatch(url, patterns)
         || configuration.getHomePageUrl().equals(url))) {
       throw new PerryException(param + ": " + url + " is not registered");
     }
@@ -30,6 +33,10 @@ public class WhiteList {
 
   private boolean disabled(List<String> whiteList) {
     return whiteList.size() == 1 && whiteList.contains("*");
+  }
+
+  private boolean anyMatch(String url, List<String> patterns) {
+    return patterns.stream().anyMatch(pattern -> pathMatcher.match(pattern, url));
   }
 
 }
