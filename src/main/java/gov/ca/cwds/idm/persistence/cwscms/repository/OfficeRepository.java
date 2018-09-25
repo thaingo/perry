daeb812f2c0668e2d38ea8123beab5b3deb9658c
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-09-25 14:33:21 -0700

    COG-616: implementation initial version

--- src/main/java/gov/ca/cwds/idm/persistence/cwscms/repository/OfficeRepository.java
+++ src/main/java/gov/ca/cwds/idm/persistence/cwscms/repository/OfficeRepository.java
@@ -6,14 +6,23 @@ import gov.ca.cwds.idm.dto.Office;
 import java.util.List;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Profile("idm")
 @Repository
 public interface OfficeRepository extends ReadOnlyRepository<CwsOffice, String> {
 
+  String COUNTY_NAME = "countyName";
+
   @Query("select new gov.ca.cwds.idm.dto.Office(office.officeId, trim(office.cwsOfficeName),"
       + " office.governmentEntityType, trim(county.shortDescription)) from CwsOffice office,"
       + " County county where office.governmentEntityType = county.systemId ")
   List<Office> findOffices();
+
+  @Query("select new gov.ca.cwds.idm.dto.Office(office.officeId, trim(office.cwsOfficeName),"
+      + " office.governmentEntityType, trim(county.shortDescription)) from CwsOffice office,"
+      + " County county where office.governmentEntityType = county.systemId "
+      + "and county.shortDescription = :" + COUNTY_NAME)
+  List<Office> findCountyOffices(@Param(COUNTY_NAME) String countyName);
 }
