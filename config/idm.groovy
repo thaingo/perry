663b2e19a42b434fe8124c7a4ad7e2296a720e9a
sorych <dmytro.sorych@osi.ca.gov>
2018-10-08 16:54:47 -0700

    COG-662 Change User's Composition Approach

--- config/idm.groovy
+++ config/idm.groovy
@@ -1,22 +1,46 @@
+import gov.ca.cwds.rest.api.domain.auth.GovernmentEntityType
+
 def attribute = {name -> cognitoUser.attributes?.find {it.name.equalsIgnoreCase(name)}?.value}
 
+if(cwsUser) {
 
-result.id = cognitoUser.username
-result.enabled = cognitoUser.enabled
-result.userCreateDate = cognitoUser.userCreateDate
-result.userLastModifiedDate = cognitoUser.userLastModifiedDate
-result.status = cognitoUser.userStatus
-result.countyName = attribute("custom:County")
-result.firstName = attribute("given_name")
-result.lastName = attribute("family_name")
-result.racfid = attribute("CUSTOM:RACFID")
-result.email = attribute("email")
-result.permissions = attribute("custom:permission")?.split('\\s*:\\s*') as HashSet
-result.roles = attribute("custom:role")?.split('\\s*:\\s*') as HashSet
+    def governmentEntityType = GovernmentEntityType.findBySysId(cwsUser.cwsOffice?.governmentEntityType)
 
-result.startDate = cwsUser?.staffPerson?.startDate
-result.endDate = cwsUser?.staffPerson?.endDate
-result.officeId = cwsUser?.cwsOffice?.officeId
-result.phoneExtensionNumber = cwsUser?.cwsOffice?.primaryPhoneExtensionNumber
-result.phoneNumber = cwsUser?.cwsOffice?.primaryPhoneNumber
+    result.id = cognitoUser.username
+    result.enabled = cognitoUser.enabled
+    result.userCreateDate = cognitoUser.userCreateDate
+    result.userLastModifiedDate = cognitoUser.userLastModifiedDate
+    result.status = cognitoUser.userStatus
+    result.racfid = attribute("CUSTOM:RACFID")
+    result.email = attribute("email")
+    result.permissions = attribute("custom:permission")?.split('\\s*:\\s*') as HashSet
+    result.roles = attribute("custom:role")?.split('\\s*:\\s*') as HashSet
+
+    result.startDate = cwsUser.staffPerson?.startDate
+    result.endDate = cwsUser.staffPerson?.endDate
+    result.countyName = governmentEntityType?.description
+    result.firstName = cwsUser.staffPerson?.firstName
+    result.lastName = cwsUser.staffPerson?.lastName
+    result.officeId = cwsUser.cwsOffice?.officeId
+    result.phoneExtensionNumber = cwsUser.cwsOffice?.primaryPhoneExtensionNumber
+    result.phoneNumber = cwsUser.cwsOffice?.primaryPhoneNumber
+
+}
+else {
+    result.id = cognitoUser.username
+    result.enabled = cognitoUser.enabled
+    result.userCreateDate = cognitoUser.userCreateDate
+    result.userLastModifiedDate = cognitoUser.userLastModifiedDate
+    result.status = cognitoUser.userStatus
+    result.countyName = attribute("custom:County")
+    result.firstName = attribute("given_name")
+    result.lastName = attribute("family_name")
+    result.racfid = attribute("CUSTOM:RACFID")
+    result.email = attribute("email")
+    result.permissions = attribute("custom:permission")?.split('\\s*:\\s*') as HashSet
+    result.roles = attribute("custom:role")?.split('\\s*:\\s*') as HashSet
+    result.officeId = attribute("custom:Office")
+
+}
+
 
