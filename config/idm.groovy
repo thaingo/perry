94ba1b5251b0b3cf3a66ffce1c43bcedc120fd9e
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-10-11 10:25:50 -0700

    COG-617: checks for blank string values of role and permission attributes are added, refactored

--- config/idm.groovy
+++ config/idm.groovy
@@ -1,20 +1,27 @@
 import gov.ca.cwds.rest.api.domain.auth.GovernmentEntityType
+import org.apache.commons.lang3.StringUtils
 
 def attribute = {name -> cognitoUser.attributes?.find {it.name.equalsIgnoreCase(name)}?.value}
+
+result.id = cognitoUser.username
+result.enabled = cognitoUser.enabled
+result.userCreateDate = cognitoUser.userCreateDate
+result.userLastModifiedDate = cognitoUser.userLastModifiedDate
+result.status = cognitoUser.userStatus
+result.email = attribute("email")
+result.racfid = attribute("CUSTOM:RACFID")
+
+if(StringUtils.isNotBlank(attribute("custom:permission"))) {
+    result.permissions = attribute("custom:permission").split('\\s*:\\s*') as HashSet
+}
+
+if(StringUtils.isNotBlank(attribute("custom:role"))) {
+    result.roles = attribute("custom:role").split('\\s*:\\s*') as HashSet
+}
 
 if(cwsUser) {
 
     def governmentEntityType = GovernmentEntityType.findBySysId(cwsUser.cwsOffice?.governmentEntityType)
-
-    result.id = cognitoUser.username
-    result.enabled = cognitoUser.enabled
-    result.userCreateDate = cognitoUser.userCreateDate
-    result.userLastModifiedDate = cognitoUser.userLastModifiedDate
-    result.status = cognitoUser.userStatus
-    result.racfid = attribute("CUSTOM:RACFID")
-    result.email = attribute("email")
-    result.permissions = attribute("custom:permission")?.split('\\s*:\\s*') as HashSet
-    result.roles = attribute("custom:role")?.split('\\s*:\\s*') as HashSet
 
     result.startDate = cwsUser.staffPerson?.startDate
     result.endDate = cwsUser.staffPerson?.endDate
@@ -25,22 +32,11 @@ if(cwsUser) {
     result.phoneExtensionNumber = cwsUser.cwsOffice?.primaryPhoneExtensionNumber
     result.phoneNumber = cwsUser.cwsOffice?.primaryPhoneNumber
 
-}
-else {
-    result.id = cognitoUser.username
-    result.enabled = cognitoUser.enabled
-    result.userCreateDate = cognitoUser.userCreateDate
-    result.userLastModifiedDate = cognitoUser.userLastModifiedDate
-    result.status = cognitoUser.userStatus
+} else {
     result.countyName = attribute("custom:County")
     result.firstName = attribute("given_name")
     result.lastName = attribute("family_name")
-    result.racfid = attribute("CUSTOM:RACFID")
-    result.email = attribute("email")
-    result.permissions = attribute("custom:permission")?.split('\\s*:\\s*') as HashSet
-    result.roles = attribute("custom:role")?.split('\\s*:\\s*') as HashSet
     result.officeId = attribute("custom:Office")
-
 }
 
 
