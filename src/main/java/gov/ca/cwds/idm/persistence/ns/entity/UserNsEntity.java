0ce03c848a6b4b330e1d4f46a99e85abe734c2e5
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-10-23 14:30:32 -0700

    COG-710: entity is created

---
+++ src/main/java/gov/ca/cwds/idm/persistence/ns/entity/UserNsEntity.java
@@ -0,0 +1,71 @@
+package gov.ca.cwds.idm.persistence.ns.entity;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "user")
+public class UserNsEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
+
+  @NotNull
+  @Size(min = 1, max = 128)
+  @Column(name = "username")
+  private String username;
+
+  @Column(name = "last_login_time")
+  private LocalDateTime lastLoginTime;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public LocalDateTime getLastLoginTime() {
+    return lastLoginTime;
+  }
+
+  public void setLastLoginTime(LocalDateTime lastLoginTime) {
+    this.lastLoginTime = lastLoginTime;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof UserNsEntity)) {
+      return false;
+    }
+    UserNsEntity that = (UserNsEntity) o;
+    return Objects.equals(getUsername(), that.getUsername());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getUsername());
+  }
+}
