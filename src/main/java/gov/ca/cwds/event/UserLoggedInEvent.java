24fd6fd8080fe9422874f053956bf7368eeb3885
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-10-24 10:53:02 -0700

    COG-710: loginTime is added to UserLoggedInEvent

--- src/main/java/gov/ca/cwds/event/UserLoggedInEvent.java
+++ src/main/java/gov/ca/cwds/event/UserLoggedInEvent.java
@@ -1,6 +1,7 @@
 package gov.ca.cwds.event;
 
 import gov.ca.cwds.UniversalUserToken;
+import java.time.LocalDateTime;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 
 /**
@@ -8,14 +9,20 @@ import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
  */
 public final class UserLoggedInEvent {
 
-  private UniversalUserToken userToken;
+  private final UniversalUserToken userToken;
+  private final LocalDateTime loginTime;
 
   public UserLoggedInEvent(UniversalUserToken userToken) {
     this.userToken = userToken;
+    this.loginTime = LocalDateTime.now();
   }
 
   public String getUserId() {
     return (String) userToken.getParameters().get("userName");
+  }
+
+  public LocalDateTime getLoginTime() {
+    return loginTime;
   }
 
   @Override
