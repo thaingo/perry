5c834a7e3f4c0b05a5b0c5edb002a3a3d1904e2f
Leonid Marushevskyi <leonid.marushevskiy@osi.ca.gov>
2018-11-14 19:13:12 -0800

    CANS-709: rollback debug log

--- Jenkinsfile-complete.groovy
+++ Jenkinsfile-complete.groovy
@@ -32,17 +32,17 @@ node('dora-slave') {
                 buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'clean jar'
             }
         }
-//        stage('Unit Tests') {
-//            buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'test jacocoTestReport', switches: '--info'
-//        }
-//        stage('SonarQube analysis') {
-//            withSonarQubeEnv('Core-SonarQube') {
-//                buildInfo = rtGradle.run buildFile: 'build.gradle', switches: '--info', tasks: 'sonarqube'
-//            }
-//        }
-//        stage('License Report') {
-//            buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'downloadLicenses'
-//        }
+        stage('Unit Tests') {
+            buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'test jacocoTestReport', switches: '--info'
+        }
+        stage('SonarQube analysis') {
+            withSonarQubeEnv('Core-SonarQube') {
+                buildInfo = rtGradle.run buildFile: 'build.gradle', switches: '--info', tasks: 'sonarqube'
+            }
+        }
+        stage('License Report') {
+            buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'downloadLicenses'
+        }
         stage('Build Docker') {
             withDockerRegistry([credentialsId: '6ba8d05c-ca13-4818-8329-15d41a089ec0']) {
                 if (params.RELEASE_PROJECT) {
@@ -55,45 +55,45 @@ node('dora-slave') {
         stage('Clean Workspace') {
             archiveArtifacts artifacts: '**/perry*.jar,readme.txt', fingerprint: true
         }
-//        stage('Deploy Cognito Mode') {
-//            sh 'cd ansible ; ansible-playbook -e NEW_RELIC_AGENT=$USE_NEWRELIC -e VERSION_NUMBER=$APP_VERSION -i $inventory deploy-perry.yml --vault-password-file ~/.ssh/vault.txt -vv'
-//            sleep(20)
-//        }
-//        stage('Cognito Integration Tests') {
-//            def gradlePropsText = """
-//            perry.health.check.url=http://10.110.12.162:9082/manage/health
-//            perry.url=${PERRY_URL}
-//            perry.username=cwds.perry.test@gmail.com
-//            perry.password=CWS4kids!
-//            perry.json={}
-//            perry.threads.count=1
-//            selenium.grid.url=
-//            validate.repeat.count=2
-//            """
-//            writeFile file: "gradle.properties", text: gradlePropsText
-//            buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'integrationTestProd --stacktrace'
-//        }
-//        stage('Deploy Dev Mode') {
-//            // TODO: Need to change Perry mode here to DEV
-//            sh 'sed -i \'s/devmode: "false"/devmode: "true"/\'  ansible/inventories/tpt2dev/group_vars/perry.yml'
-//            sh 'sed -i \'s/cognito_mode: "true"/cognito_mode: "false"/\'  ansible/inventories/tpt2dev/group_vars/perry.yml'
-//            sh 'cd ansible ; ansible-playbook -e NEW_RELIC_AGENT=$USE_NEWRELIC -e VERSION_NUMBER=$APP_VERSION -i $inventory deploy-perry.yml --vault-password-file ~/.ssh/vault.txt -vv'
-//            sleep(20)
-//        }
-//        stage('Dev Integration Tests') {
-//            def gradlePropsText = """
-//            perry.health.check.url=http://10.110.12.162:9082/manage/health
-//            perry.url=${PERRY_URL}
-//            perry.username=
-//            perry.password=
-//            perry.json=
-//            perry.threads.count=1
-//            selenium.grid.url=http://grid.dev.cwds.io:4444/wd/hub
-//            validate.repeat.count=2
-//            """
-//            writeFile file: "gradle.properties", text: gradlePropsText
-//            buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'integrationTestDev --stacktrace'
-//        }
+        stage('Deploy Cognito Mode') {
+            sh 'cd ansible ; ansible-playbook -e NEW_RELIC_AGENT=$USE_NEWRELIC -e VERSION_NUMBER=$APP_VERSION -i $inventory deploy-perry.yml --vault-password-file ~/.ssh/vault.txt -vv'
+            sleep(20)
+        }
+        stage('Cognito Integration Tests') {
+            def gradlePropsText = """
+            perry.health.check.url=http://10.110.12.162:9082/manage/health
+            perry.url=${PERRY_URL}
+            perry.username=cwds.perry.test@gmail.com
+            perry.password=CWS4kids!
+            perry.json={}
+            perry.threads.count=1
+            selenium.grid.url=
+            validate.repeat.count=2
+            """
+            writeFile file: "gradle.properties", text: gradlePropsText
+            buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'integrationTestProd --stacktrace'
+        }
+        stage('Deploy Dev Mode') {
+            // TODO: Need to change Perry mode here to DEV
+            sh 'sed -i \'s/devmode: "false"/devmode: "true"/\'  ansible/inventories/tpt2dev/group_vars/perry.yml'
+            sh 'sed -i \'s/cognito_mode: "true"/cognito_mode: "false"/\'  ansible/inventories/tpt2dev/group_vars/perry.yml'
+            sh 'cd ansible ; ansible-playbook -e NEW_RELIC_AGENT=$USE_NEWRELIC -e VERSION_NUMBER=$APP_VERSION -i $inventory deploy-perry.yml --vault-password-file ~/.ssh/vault.txt -vv'
+            sleep(20)
+        }
+        stage('Dev Integration Tests') {
+            def gradlePropsText = """
+            perry.health.check.url=http://10.110.12.162:9082/manage/health
+            perry.url=${PERRY_URL}
+            perry.username=
+            perry.password=
+            perry.json=
+            perry.threads.count=1
+            selenium.grid.url=http://grid.dev.cwds.io:4444/wd/hub
+            validate.repeat.count=2
+            """
+            writeFile file: "gradle.properties", text: gradlePropsText
+            buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'integrationTestDev --stacktrace'
+        }
         stage('Push artifacts') {
             // Artifactory
             rtGradle.deployer.deployArtifacts = true
