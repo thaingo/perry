71ccc270406018bbc2ba727e5bcc635b5188afb9
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-12-04 14:54:50 -0800

    COG-628: CANS is excluded from possible permissions if user does not have RacfId

--- src/main/java/gov/ca/cwds/idm/service/role/implementor/AbstractAdminRoleImplementor.java
+++ src/main/java/gov/ca/cwds/idm/service/role/implementor/AbstractAdminRoleImplementor.java
@@ -1,26 +1,33 @@
 package gov.ca.cwds.idm.service.role.implementor;
 
+import static java.util.stream.Collectors.toList;
+
 import gov.ca.cwds.idm.persistence.ns.entity.Permission;
 import gov.ca.cwds.idm.service.DictionaryProvider;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public abstract class AbstractAdminRoleImplementor implements AdminRoleImplementor {
+
+  private static final String CANS_PERMISSION_NAME = "CANS-rollout";
 
   private DictionaryProvider dictionaryProvider;
 
   @Override
   public List<String> getPossibleUserPermissions(boolean isRacfidUser) {
+    List<String> allPermissionNames = getAllPermissionNames();
+
     if(isRacfidUser) {
-      return getAllPermissionNames();
+      return allPermissionNames;
     } else {
-      return getAllPermissionNames();
+      return allPermissionNames.stream()
+          .filter(name -> !name.equals(CANS_PERMISSION_NAME))
+          .collect(toList());
     }
   }
 
   private List<String> getAllPermissionNames() {
     return dictionaryProvider.getPermissions().stream()
-        .map(Permission::getName).collect(Collectors.toList());
+        .map(Permission::getName).collect(toList());
   }
 
   public void setDictionaryProvider(DictionaryProvider dictionaryProvider) {
