5666656bb64c36b1c67c666c4e28e03c11c34bbb
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-12-17 10:49:56 -0800

    COG-582: SUPER_ADMIN case is added

--- src/main/java/gov/ca/cwds/idm/service/role/implementor/AdminRoleImplementorFactory.java
+++ src/main/java/gov/ca/cwds/idm/service/role/implementor/AdminRoleImplementorFactory.java
@@ -4,6 +4,7 @@ import static gov.ca.cwds.config.api.idm.Roles.CALS_ADMIN;
 import static gov.ca.cwds.config.api.idm.Roles.COUNTY_ADMIN;
 import static gov.ca.cwds.config.api.idm.Roles.OFFICE_ADMIN;
 import static gov.ca.cwds.config.api.idm.Roles.STATE_ADMIN;
+import static gov.ca.cwds.config.api.idm.Roles.SUPER_ADMIN;
 import static gov.ca.cwds.util.CurrentAuthenticatedUserUtil.getCurrentUser;
 
 import gov.ca.cwds.idm.dto.User;
@@ -22,6 +23,8 @@ public class AdminRoleImplementorFactory {
   private AdminRoleImplementor createAdminRoleImplementor() {
 
     switch (UserRolesService.getStrongestAdminRole(getCurrentUser())) {
+      case SUPER_ADMIN:
+        return new SuperAdminRoleImplementor();
       case STATE_ADMIN:
         return new StateAdminRoleImplementor();
       case COUNTY_ADMIN:
