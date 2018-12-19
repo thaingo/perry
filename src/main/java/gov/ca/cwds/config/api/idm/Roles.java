229554693f42682b6ae3771cf4b4b89ba81f32e9
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-12-19 10:32:16 -0800

    COG-582: unused import is removed

--- src/main/java/gov/ca/cwds/config/api/idm/Roles.java
+++ src/main/java/gov/ca/cwds/config/api/idm/Roles.java
@@ -4,7 +4,6 @@ import static gov.ca.cwds.util.Utils.toSet;
 
 import com.google.common.collect.ImmutableMap;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
