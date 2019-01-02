f4fad83023362fe94cffd77a4e1706f8af8c0170
Adarsh Vandana <adarsh.vandana@osi.ca.gov>
2019-01-02 09:53:20 -0800

    Added the trigger properties from library

--- Jenkinsfile-pull-request.groovy
+++ Jenkinsfile-pull-request.groovy
@@ -13,7 +13,11 @@ node('dora-slave') {
                         string(defaultValue: "", description: 'Fill this field if need to specify custom version ', name: 'OVERRIDE_VERSION'),
                         booleanParam(defaultValue: true, description: 'Enable NewRelic APM', name: 'USE_NEWRELIC'),
                         string(defaultValue: 'inventories/tpt2dev/hosts.yml', description: '', name: 'inventory')
-                ]), pipelineTriggers([pollSCM('H/5 * * * *')])
+                ])
+                triggerProperties = githubPullRequestBuilderTriggerProperties()
+                properties([
+                pipelineTriggers([triggerProperties])
+  ]))
                ])
     try {
         stage('Preparation') {
