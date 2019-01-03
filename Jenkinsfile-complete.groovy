95b73ef0ff55ab94755dc6707ee50a140b0bd850
Adarsh Vandana <adarsh.vandana@osi.ca.gov>
2019-01-03 13:25:46 -0800

    reverted the Jenkins file and pushing the gradle file

--- Jenkinsfile-complete.groovy
+++ Jenkinsfile-complete.groovy
@@ -1,34 +1,19 @@
-@Library('jenkins-pipeline-utils') _
-
 node('dora-slave') {
     def serverArti = Artifactory.server 'CWDS_DEV'
     def rtGradle = Artifactory.newGradleBuild()
-    def github_credentials_id = '433ac100-b3c2-4519-b4d6-207c029a103b'
-    newTag = '';
     properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), disableConcurrentBuilds(), [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false],
                 parameters([
+                        string(defaultValue: 'SNAPSHOT', description: 'Release version (if not SNAPSHOT will be released to lib-release repository)', name: 'VERSION'),
                         string(defaultValue: 'latest', description: '', name: 'APP_VERSION'),
                         string(defaultValue: 'master', description: 'perry branch', name: 'branch'),
                         string(defaultValue: 'master', description: 'ansible branch', name: 'ansible_branch'),
                         string(defaultValue: '', description: 'Used for mergerequest default is empty', name: 'refspec'),
+                        booleanParam(defaultValue: true, description: 'Default release version template is: <majorVersion>_<buildNumber>-RC', name: 'RELEASE_PROJECT'),
+                        string(defaultValue: "", description: 'Fill this field if need to specify custom version ', name: 'OVERRIDE_VERSION'),
                         booleanParam(defaultValue: true, description: 'Enable NewRelic APM', name: 'USE_NEWRELIC'),
                         string(defaultValue: 'inventories/tpt2dev/hosts.yml', description: '', name: 'inventory'),
                         string(defaultValue: 'https://web.dev.cwds.io/perry', description: 'Perry base URL', name: 'PERRY_URL'),
-                ]),
-               pipelineTriggers([
-                [$class: 'GenericTrigger',
-                  genericVariables: [
-                    [key: 'pull_request_action', value: 'action', expressionType: 'JSONPath'],
-                    [key: 'pull_request_merged', value: 'pull_request.merged', expressionType: 'JSONPath'],
-                    [key: 'pull_request_event', value: 'pull_request', expressionType: 'JSONPath']
-                    ],
-                  causeString: 'Triggered by a PR merge',
-                  token: 'perry-master',
-                  regexpFilterText: '$pull_request_action:$pull_request_merged',
-                  regexpFilterExpression: '^closed:true$'
-                ]
-               ])
-             ])
+                ]), pipelineTriggers([pollSCM('H/5 * * * *')])])
     try {
         stage('Preparation') {
             cleanWs()
@@ -39,10 +24,13 @@ node('dora-slave') {
             rtGradle.useWrapper = true
         }
         stage('Build') {
-            buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'clean jar'
-        }
-        stage('Increment Tag') {
-          newTag = newSemVer()
+            if (params.RELEASE_PROJECT) {
+                echo "!!!! BUILD RELEASE VERSION"
+                buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'clean jar -DRelease=$RELEASE_PROJECT -DBuildNumber=$BUILD_NUMBER -DCustomVersion=$OVERRIDE_VERSION'
+            } else {
+                echo "!!!! BUILD SNAPSHOT VERSION"
+                buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'clean jar'
+            }
         }
         stage('Unit Tests') {
             buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'test jacocoTestReport', switches: '--info'
@@ -57,7 +45,11 @@ node('dora-slave') {
         }
         stage('Build Docker') {
             withDockerRegistry([credentialsId: '6ba8d05c-ca13-4818-8329-15d41a089ec0']) {
-                buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'publishLatestDocker'
+                if (params.RELEASE_PROJECT) {
+                    buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'publishLatestDocker -DRelease=$RELEASE_PROJECT -DBuildNumber=$BUILD_NUMBER -DCustomVersion=$OVERRIDE_VERSION'
+                } else {
+                    buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'publishLatestDocker -DRelease=false -DBuildNumber=$BUILD_NUMBER'
+                }
             }
         }
         stage('Clean Workspace') {
@@ -104,15 +96,34 @@ node('dora-slave') {
         stage('Push artifacts') {
             // Artifactory
             rtGradle.deployer.deployArtifacts = true
-            buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: "publish -DnewVersion=${newTag}".toString()
+            if (params.RELEASE_PROJECT) {
+                echo "!!!! PUSH RELEASE VERSION ${params.VERSION}"
+                buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'publish -DRelease=$RELEASE_PROJECT -DBuildNumber=$BUILD_NUMBER -DCustomVersion=$OVERRIDE_VERSION'
+            } else {
+                echo "!!!! PUSH SNAPSHOT VERSION"
+                buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'publish'
+            }
             rtGradle.deployer.deployArtifacts = false
             // Docker Hub
             withDockerRegistry([credentialsId: '6ba8d05c-ca13-4818-8329-15d41a089ec0']) {
-                buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: "publishDocker -DRelease=false -DnewVersion=${newTag}".toString()
+                if (params.RELEASE_PROJECT) {
+                    buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'publishDocker -DRelease=$RELEASE_PROJECT -DBuildNumber=$BUILD_NUMBER -DCustomVersion=$OVERRIDE_VERSION'
+                } else {
+                    buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'publishDocker -DRelease=false -DBuildNumber=$BUILD_NUMBER'
+                }
             }
         }
         stage('Tag Git') {
-          tagGithubRepo(newTag, github_credentials_id)
+            sshagent (credentials: ['433ac100-b3c2-4519-b4d6-207c029a103b']) {
+                if (params.RELEASE_PROJECT) {
+                    echo "!!!! BUILD RELEASE VERSION"
+                    // tagRepo('test-tags')
+                    buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'pushGitTag -DRelease=$RELEASE_PROJECT -DBuildNumber=$BUILD_NUMBER -DCustomVersion=$OVERRIDE_VERSION'
+                } else {
+                    echo "!!!! BUILD SNAPSHOT VERSION"
+                    buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'pushGitTag -DRelease=$RELEASE_PROJECT -DBuildNumber=$BUILD_NUMBER -DCustomVersion=$OVERRIDE_VERSION'
+                }
+            }
         }
         stage('Trigger Security scan') {
             def props = readProperties  file: 'build/resources/main/version.properties'
