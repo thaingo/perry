ef630dfe6827cb293447ba7c712bda5aa82c4d19
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2019-02-22 16:56:13 -0800

    COG-1117: mapping for racfid is changed

--- config/idm.groovy
+++ config/idm.groovy
@@ -3,16 +3,17 @@ import gov.ca.cwds.idm.service.cognito.util.CognitoPhoneConverter
 import org.apache.commons.lang3.StringUtils
 import gov.ca.cwds.util.Utils
 
-def attribute = {name -> cognitoUser.attributes?.find {it.name.equalsIgnoreCase(name)}?.value}
+def cognitoUserAttribute = {name -> cognitoUser.attributes?.find {it.name.equalsIgnoreCase(name)}?.value}
 
 result.id = cognitoUser.username
 result.enabled = cognitoUser.enabled
 result.userCreateDate = cognitoUser.userCreateDate
 result.status = cognitoUser.userStatus
-result.email = attribute("email")
-result.racfid = attribute("custom:RACFID")
-result.phoneNumber = CognitoPhoneConverter.fromCognitoFormat(attribute("phone_number"))
-result.phoneExtensionNumber = attribute("custom:PhoneExtension")
+result.email = cognitoUserAttribute("email")
+result.phoneNumber = CognitoPhoneConverter.fromCognitoFormat(cognitoUserAttribute("phone_number"))
+result.phoneExtensionNumber = cognitoUserAttribute("custom:PhoneExtension")
+
+result.racfid = nsUser.racfid
 result.lastLoginDateTime = nsUser.lastLoginTime
 result.notes = nsUser.notes
 
@@ -22,16 +23,16 @@ long cognitoModifiedMillis = cognitoModifiedTime?.time?:0
 long nsDbModifiedMillis = nsDbModifiedTime?.time?:0
 result.userLastModifiedDate = nsDbModifiedMillis > cognitoModifiedMillis ? nsDbModifiedTime : cognitoModifiedTime
 
-if(StringUtils.isNotBlank(attribute("custom:locked"))) {
-    result.locked = attribute("custom:locked").toBoolean()
+if(StringUtils.isNotBlank(cognitoUserAttribute("custom:locked"))) {
+    result.locked = cognitoUserAttribute("custom:locked").toBoolean()
 }
 
-if(StringUtils.isNotBlank(attribute("custom:Permission"))) {
-    result.permissions = attribute("custom:Permission").split('\\s*:\\s*') as HashSet
+if(StringUtils.isNotBlank(cognitoUserAttribute("custom:Permission"))) {
+    result.permissions = cognitoUserAttribute("custom:Permission").split('\\s*:\\s*') as HashSet
 }
 
-if(StringUtils.isNotBlank(attribute("custom:Role"))) {
-    result.roles = attribute("custom:Role").split('\\s*:\\s*') as HashSet
+if(StringUtils.isNotBlank(cognitoUserAttribute("custom:Role"))) {
+    result.roles = cognitoUserAttribute("custom:Role").split('\\s*:\\s*') as HashSet
 }
 
 if(cwsUser) {
@@ -48,10 +49,10 @@ if(cwsUser) {
     result.officePhoneExtensionNumber = cwsUser.cwsOffice?.primaryPhoneExtensionNumber
     result.cwsPrivileges = cwsUser.cwsStaffPrivs
 } else {
-    result.countyName = attribute("custom:County")
-    result.firstName = attribute("given_name")
-    result.lastName = attribute("family_name")
-    result.officeId = attribute("custom:Office")
+    result.countyName = cognitoUserAttribute("custom:County")
+    result.firstName = cognitoUserAttribute("given_name")
+    result.lastName = cognitoUserAttribute("family_name")
+    result.officeId = cognitoUserAttribute("custom:Office")
 }
 
 
