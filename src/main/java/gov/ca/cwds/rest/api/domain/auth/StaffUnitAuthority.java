e284407a2acae44cf9270d428ab341ee5c1eb898
Oleksii Khilkevych <oleksii.khilkevych@osi.ca.gov>
2019-03-11 15:08:46 -0700

    FIT-485 fix critical code smells.

--- src/main/java/gov/ca/cwds/rest/api/domain/auth/StaffUnitAuthority.java
+++ src/main/java/gov/ca/cwds/rest/api/domain/auth/StaffUnitAuthority.java
@@ -5,7 +5,8 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 /**
  * Class representing a Staff Person Unit of Authority.
@@ -124,96 +125,14 @@ public class StaffUnitAuthority {
     return endDate;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    return EqualsBuilder.reflectionEquals(this, o);
+  }
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see java.lang.Object#hashCode()
-   */
   @Override
   public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((assignedUnit == null) ? 0 : assignedUnit.hashCode());
-    result = prime * result + ((assignedUnitEndDate == null) ? 0 : assignedUnitEndDate.hashCode());
-    result = prime * result + ((county == null) ? 0 : county.hashCode());
-    result = prime * result + ((countyCode == null) ? 0 : countyCode.hashCode());
-    result = prime * result + ((endDate == null) ? 0 : endDate.hashCode());
-    result = prime * result + ((unitAuthorityCode == null) ? 0 : unitAuthorityCode.hashCode());
-    result =
-            prime * result + ((unitAuthorityCodeDesc == null) ? 0 : unitAuthorityCodeDesc.hashCode());
-    return result;
+    return HashCodeBuilder.reflectionHashCode(this);
   }
-
-
-  /*
-   * (non-Javadoc)
-   * 
-   * @see java.lang.Object#equals(java.lang.Object)
-   */
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
-    }
-    if (obj == null) {
-      return false;
-    }
-    if (getClass() != obj.getClass()) {
-      return false;
-    }
-    StaffUnitAuthority other = (StaffUnitAuthority) obj;
-    if (assignedUnit == null) {
-      if (other.assignedUnit != null) {
-        return false;
-      }
-    } else if (!assignedUnit.equals(other.assignedUnit)) {
-      return false;
-    }
-    if (assignedUnitEndDate == null) {
-      if (other.assignedUnitEndDate != null) {
-        return false;
-      }
-    } else if (!assignedUnitEndDate.equals(other.assignedUnitEndDate)) {
-      return false;
-    }
-    if (county == null) {
-      if (other.county != null) {
-        return false;
-      }
-    } else if (!county.equals(other.county)) {
-      return false;
-    }
-    if (endDate == null) {
-      if (other.endDate != null) {
-        return false;
-      }
-    } else if (!endDate.equals(other.endDate)) {
-      return false;
-    }
-    if (countyCode == null) {
-      if (other.countyCode != null) {
-        return false;
-      }
-    } else if (!countyCode.equals(other.countyCode)) {
-      return false;
-    }
-    if (unitAuthorityCode == null) {
-      if (other.unitAuthorityCode != null) {
-        return false;
-      }
-    } else if (!unitAuthorityCode.equals(other.unitAuthorityCode)) {
-      return false;
-    }
-    if (unitAuthorityCodeDesc == null) {
-      if (other.unitAuthorityCodeDesc != null) {
-        return false;
-      }
-    } else if (!unitAuthorityCodeDesc.equals(other.unitAuthorityCodeDesc)) {
-      return false;
-    }
-    return true;
-  }
-
 
 }
