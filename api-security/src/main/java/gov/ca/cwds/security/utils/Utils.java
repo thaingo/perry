e8f543f08b05020f035d1fc4f1c316fd4566d1f0
Naga Pradeep Gajula <naga.gajula@osi.ca.gov>
2019-03-12 09:58:27 -0700

    FIT-486 Add a private constructor to hide the implicit public one

--- api-security/src/main/java/gov/ca/cwds/security/utils/Utils.java
+++ api-security/src/main/java/gov/ca/cwds/security/utils/Utils.java
@@ -4,6 +4,11 @@ package gov.ca.cwds.security.utils;
  * Created by dmitry.rudenko on 9/29/2017.
  */
 public class Utils {
+
+  private UtilsClass() {
+      throw new IllegalStateException("Utility class");
+  }
+
   public static String replaceCRLF(String string) {
     return string.replaceAll("[\\n\\r]", " ");
   }
