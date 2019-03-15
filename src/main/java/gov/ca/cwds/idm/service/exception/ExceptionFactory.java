41bbbcf1ff8337bf805bdaee121e63f4e2fd02ff
sorych <dmytro.sorych@osi.ca.gov>
2019-03-14 21:27:55 -0700

    NsUserService and Repository moved to prod profilegit add -u

--- src/main/java/gov/ca/cwds/idm/service/exception/ExceptionFactory.java
+++ src/main/java/gov/ca/cwds/idm/service/exception/ExceptionFactory.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("idm")
+@Profile({"prod"})
 public class ExceptionFactory {
 
   private MessagesService messagesService;
