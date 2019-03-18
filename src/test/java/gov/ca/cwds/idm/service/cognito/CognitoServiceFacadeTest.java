38ea110a8a3d7c1df03cdf57eff75930aa552dc3
Kevin Fries <kelfink@yahoo.com>
2019-03-18 16:53:26 -0700

    Code style

--- src/test/java/gov/ca/cwds/idm/service/cognito/CognitoServiceFacadeTest.java
+++ src/test/java/gov/ca/cwds/idm/service/cognito/CognitoServiceFacadeTest.java
@@ -54,7 +54,14 @@ import gov.ca.cwds.service.messages.MessageCode;
 import gov.ca.cwds.service.messages.MessagesService;
 import gov.ca.cwds.service.messages.MessagesService.Messages;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.hamcrest.junit.ExpectedException;
