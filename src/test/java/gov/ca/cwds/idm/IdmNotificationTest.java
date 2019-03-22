236d1cf65d664bdbeba54d96f137c2f559b7d269
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2019-03-22 15:27:21 -0700

    COG-1335: test is changed because of synchronous user index updating

--- src/test/java/gov/ca/cwds/idm/IdmNotificationTest.java
+++ src/test/java/gov/ca/cwds/idm/IdmNotificationTest.java
@@ -1,5 +1,6 @@
 package gov.ca.cwds.idm;
 
+import static gov.ca.cwds.config.TokenServiceConfiguration.TOKEN_TRANSACTION_MANAGER;
 import static gov.ca.cwds.idm.service.notification.NotificationTypes.USER_LOCKED;
 import static gov.ca.cwds.idm.util.TestCognitoServiceFacade.ABSENT_USER_ID;
 import static gov.ca.cwds.idm.util.TestCognitoServiceFacade.NEW_USER_SUCCESS_ID;
@@ -7,26 +8,35 @@ import static gov.ca.cwds.idm.util.TestUtils.asJsonString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-import com.google.common.collect.Iterables;
 import gov.ca.cwds.idm.dto.IdmNotification;
+import gov.ca.cwds.idm.dto.User;
 import gov.ca.cwds.idm.event.UserLockedEvent;
 import org.apache.commons.codec.binary.Base64;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
 
 public class IdmNotificationTest extends BaseIdmIntegrationWithSearchTest {
+
+  @Before
+  public void before() {
+    super.before();
+    setDoraSuccess();
+  }
 
   @Test
   public void testNotifyUserWasLocked() throws Exception {
 
     IdmNotification notification = new IdmNotification(NEW_USER_SUCCESS_ID, USER_LOCKED);
-    int oldUserLogsSize = Iterables.size(userLogRepository.findAll());
+    long oldUserLogsSize = userLogRepository.count();
 
     mockMvc
         .perform(MockMvcRequestBuilders.post("/idm/notifications/")
@@ -38,8 +48,33 @@ public class IdmNotificationTest extends BaseIdmIntegrationWithSearchTest {
 
     verify(auditEventService, times(1)).persistAuditEvent(any(
         UserLockedEvent.class));
-    int newUserLogsSize = Iterables.size(userLogRepository.findAll());
-    assertThat(newUserLogsSize, is(oldUserLogsSize + 1));
+
+    verify(spySearchService, times(1)).updateUser(argThat(User::isLocked));
+
+    assertThat(userLogRepository.count(), is(oldUserLogsSize));
+  }
+
+  @Test
+  @Transactional(value = TOKEN_TRANSACTION_MANAGER)
+  public void testNotifyUserWasLockedDoraError() throws Exception {
+
+    IdmNotification notification = new IdmNotification(NEW_USER_SUCCESS_ID, USER_LOCKED);
+    long oldUserLogsSize = userLogRepository.count();
+
+    setDoraError();
+
+    mockMvc
+        .perform(MockMvcRequestBuilders.post("/idm/notifications/")
+            .header(HttpHeaders.AUTHORIZATION, BASIC_AUTH_HEADER)
+            .contentType(JSON_CONTENT_TYPE)
+            .content(asJsonString(notification)))
+        .andExpect(MockMvcResultMatchers.status().isAccepted())
+        .andReturn();
+
+    verify(auditEventService, times(1)).persistAuditEvent(any(
+        UserLockedEvent.class));
+
+    assertThat(userLogRepository.count(), is(oldUserLogsSize + 1));
   }
 
   @Test
