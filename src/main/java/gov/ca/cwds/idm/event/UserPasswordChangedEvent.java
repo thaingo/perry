09f2056edafa73dd29f34a5204f46abb24201e92
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2019-04-09 14:48:19 -0700

    COG-1386: Sonar false positive is suppressed

--- src/main/java/gov/ca/cwds/idm/event/UserPasswordChangedEvent.java
+++ src/main/java/gov/ca/cwds/idm/event/UserPasswordChangedEvent.java
@@ -7,6 +7,7 @@ import gov.ca.cwds.idm.dto.User;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@SuppressWarnings({"squid:S2068"})
 public class UserPasswordChangedEvent extends UserCausedChangeLogEvent {
 
   public static final String EVENT_TYPE_USER_PASSWORD_CHANGED = "User Password Changed";
