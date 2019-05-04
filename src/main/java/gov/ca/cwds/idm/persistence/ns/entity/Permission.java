24a2fe7861f766f7155631670227402f45ac04ac
sorych <dmytro.sorych@osi.ca.gov>
2019-05-03 18:37:19 -0700

    Permissions: 'hidden' column and field added

--- src/main/java/gov/ca/cwds/idm/persistence/ns/entity/Permission.java
+++ src/main/java/gov/ca/cwds/idm/persistence/ns/entity/Permission.java
@@ -1,5 +1,6 @@
 package gov.ca.cwds.idm.persistence.ns.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.io.Serializable;
 import java.util.Objects;
 import javax.persistence.Cacheable;
@@ -8,13 +9,15 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 @Entity
 @Cacheable
 @Table(name = "permission")
 public class Permission  implements Serializable {
 
-  private static final long serialVersionUID = -1871160666991025171L;
+  private static final long serialVersionUID = 630567911516659326L;
 
   @Id
   @NotNull
@@ -24,6 +27,10 @@ public class Permission  implements Serializable {
   @NotNull
   @Column(name = "description")
   private String description;
+
+  @JsonIgnore
+  @Column(name = "hidden")
+  private boolean hidden;
 
   public Permission() {
     //empty
@@ -50,21 +57,39 @@ public class Permission  implements Serializable {
     this.description = description;
   }
 
+  public boolean isHidden() {
+    return hidden;
+  }
+
+  public void setHidden(boolean hidden) {
+    this.hidden = hidden;
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) {
       return true;
     }
-    if (!(o instanceof Permission)) {
+
+    if (o == null || getClass() != o.getClass()) {
       return false;
     }
-    Permission permission = (Permission) o;
-    return Objects.equals(getName(), permission.getName()) &&
-        Objects.equals(getDescription(), permission.getDescription());
+
+    Permission that = (Permission) o;
+
+    return new EqualsBuilder()
+        .append(hidden, that.hidden)
+        .append(name, that.name)
+        .append(description, that.description)
+        .isEquals();
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(getName(), getDescription());
+    return new HashCodeBuilder(17, 37)
+        .append(name)
+        .append(description)
+        .append(hidden)
+        .toHashCode();
   }
 }
