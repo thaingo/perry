48a82f89365c1dc7e561e3cde34688c5daac3bec
sorych <dmytro.sorych@osi.ca.gov>
2019-05-04 15:13:00 -0700

    logic around hidden permissions added

---
+++ src/main/java/gov/ca/cwds/idm/service/PermissionNames.java
@@ -0,0 +1,11 @@
+package gov.ca.cwds.idm.service;
+
+public class PermissionNames {
+
+  private PermissionNames() {
+
+  }
+
+  public static final String CANS_PERMISSION_NAME = "CANS-rollout";
+
+}
