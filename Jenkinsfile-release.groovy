939f8d4eb07fd8b61d940d8a3a78cc77225ba22e
Ed Gibbs <edward_gibbs@yahoo.com>
2019-07-12 16:55:27 -0700

    Removes preint deploy :unicorn:

--- Jenkinsfile-release.groovy
+++ Jenkinsfile-release.groovy
@@ -10,7 +10,6 @@ releasePipeline();
 
 def releasePipeline() {
   try {
-    deployWithSmoke('preint')
     deployWithSmoke('integration')
   } catch(Exception exception) {
     currentBuild.result = "FAILURE"
@@ -32,37 +31,20 @@ def smokeTestStage(environment) {
     rtGradle.tool = "Gradle_35"
     rtGradle.resolver server: serverArti
     rtGradle.useWrapper = true
-    if (environment == 'preint') {
-      def gradlePropsText = """
-        perry.health.check.url=https://web.preint.cwds.io/perry/system-information
-        perry.url=https://web.preint.cwds.io/perry
-        perry.username=
-        perry.password=
-        perry.json= { "user": "RACFID", "staffId": "0X5", "roles": [ "CWS-admin", "County-admin", "Supervisor" ], "county_code": "56", "county_cws_code": "1123", "county_name": "Ventura", "first_name": "Anna", "last_name": "Smith", "privileges": [ "CWS Case Management System", "Resource Management", "Resource Mgmt Placement Facility Maint", "Sealed", "Sensitive Persons", "Snapshot-rollout", "Hotline-rollout", "Facility-search-rollout", "RFA-rollout", "CANS-rollout", "CANS-staff-person-subordinates-read", "CANS-staff-person-read", "CANS-staff-person-clients-read", "CANS-client-read", "CANS-client-search", "CANS-assessment-read", "CANS-assessment-create", "CANS-assessment-in-progress-update", "CANS-assessment-completed-update", "CANS-assessment-in-progress-delete", "CANS-assessment-complete", "development-not-in-use" ] }
-        perry.threads.count=5
-        selenium.grid.url=
-        validate.repeat.count=2
-      """
-       writeFile file: "gradle.properties", text: gradlePropsText
-       sh "docker build --file docker/DockerfileIntegrationTestDev -t testperry ."
-       def buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'clean smokeTest'
-       publishHTML([allowMissing: true, alwaysLinkToLastBuild: true, keepAll: true, reportDir: 'build/reports/tests/integrationTest', reportFiles: 'index.html', reportName: 'Pre Integration Test Report', reportTitles: ''])
-    } else {
-       def gradlePropsText = """
-         perry.health.check.url=https://web.integration.cwds.io/perry/system-information
-         perry.url=https://web.integration.cwds.io/perry
-         perry.username=
-         perry.password=
-         perry.json={}
-         perry.threads.count=5
-         selenium.grid.url=
-         validate.repeat.count=2
-       """
-        writeFile file: "gradle.properties", text: gradlePropsText
-        sh "docker build --file docker/DockerfileIntegrationTest -t testperry ."
-        def buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'clean smokeTest'
-        publishHTML([allowMissing: true, alwaysLinkToLastBuild: true, keepAll: true, reportDir: 'build/reports/tests/smokeTest', reportFiles: 'index.html', reportName: 'Integration Test Report', reportTitles: ''])
-     }
+    def gradlePropsText = """
+      perry.health.check.url=https://web.integration.cwds.io/perry/system-information
+      perry.url=https://web.integration.cwds.io/perry
+      perry.username=
+      perry.password=
+      perry.json={}
+      perry.threads.count=5
+      selenium.grid.url=
+      validate.repeat.count=2
+    """
+    writeFile file: "gradle.properties", text: gradlePropsText
+    sh "docker build --file docker/DockerfileIntegrationTest -t testperry ."
+    def buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'clean smokeTest'
+    publishHTML([allowMissing: true, alwaysLinkToLastBuild: true, keepAll: true, reportDir: 'build/reports/tests/smokeTest', reportFiles: 'index.html', reportName: 'Integration Test Report', reportTitles: ''])
   }
 }
 
