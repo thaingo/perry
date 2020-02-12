6339b1be437726a473c468df0a4e9c9daf011f77
David R. Smith <David.Smith@osi.ca.gov>
2020-02-12 14:31:20 -0800

    CMO-288: Jetty HTTP Trace/Track

--- src/main/java/gov/ca/cwds/config/api/idm/PerryIdmAdminConfiguration.java
+++ src/main/java/gov/ca/cwds/config/api/idm/PerryIdmAdminConfiguration.java
@@ -5,12 +5,14 @@ import static gov.ca.cwds.config.api.idm.Roles.OFFICE_ADMIN;
 import static gov.ca.cwds.config.api.idm.Roles.STATE_ADMIN;
 import static gov.ca.cwds.config.api.idm.Roles.SUPER_ADMIN;
 
-import gov.ca.cwds.config.api.common.BaseApiConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import gov.ca.cwds.config.api.common.BaseApiConfiguration;
 
 @Configuration
 @Order(2)
@@ -20,10 +22,14 @@ public class PerryIdmAdminConfiguration extends BaseApiConfiguration {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.antMatcher("/idm/permissions/**")
-        .authorizeRequests()
-        .anyRequest()
-        .hasAnyAuthority(SUPER_ADMIN, STATE_ADMIN, COUNTY_ADMIN, OFFICE_ADMIN);
+    //@formatter:off
+    http
+      .antMatcher("/idm/permissions/**")
+      .authorizeRequests()
+      .antMatchers(HttpMethod.TRACE, "/**").denyAll()
+      .anyRequest().hasAnyAuthority(SUPER_ADMIN, STATE_ADMIN, COUNTY_ADMIN, OFFICE_ADMIN);
     super.configure(http);
+    //@formatter:on
   }
+
 }
